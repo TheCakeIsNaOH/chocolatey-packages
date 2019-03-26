c696bbaea36e95fedc15ffe9365314aeb3ef7469
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-03-25 21:31:20 -0500

    Updated winbtrfs
    Former-commit-id: 7fcb08c3363b5af9e935588e6ca9946915642182

--- WinBtrfs/tools/chocolateyinstall.ps1
+++ WinBtrfs/tools/chocolateyinstall.ps1
@@ -10,36 +10,36 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Internal packages (organizations) or software that has redistribution rights (community repo)
 # - Use `Install-ChocolateyInstallPackage` instead of `Install-ChocolateyPackage`
 #   and put the binaries directly into the tools folder (we call it embedding)
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
+$fileLocation = Join-Path $toolsDir 'btrfs-1.1.zip'
 # If embedding binaries increase total nupkg size to over 1GB, use share location or download from urls
 #$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
 # Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
 # Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = '' # download url, HTTPS preferred
-$url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+#$url        = '' # download url, HTTPS preferred
+#$url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
-  url           = $url
-  url64bit      = $url64
+  #unzipLocation = $toolsDir
+  #fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+  #url           = $url
+  #url64bit      = $url64
   #file         = $fileLocation
 
-  softwareName  = 'WinBtrfs*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  #softwareName  = 'WinBtrfs*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
   # Checksums are now required as of 0.10.0.
   # To determine checksums, you can get that from the original site if provided. 
   # You can also use checksum.exe (choco install checksum) and use it 
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
+  #checksum      = ''
+  #checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  #checksum64    = ''
+  #checksumType64= 'sha256' #default is checksumType
 
   # MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
+  #silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  #validExitCodes= @(0, 3010, 1641)
   # OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   #silentArgs   = '/S'           # NSIS
@@ -56,7 +56,26 @@ $packageArgs = @{
   #validExitCodes= @(0) #please insert other valid exit codes here
 }
 
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
+Get-ChocolateyUnzip $fileLocation $toolsDir
+
+Write-Host "Removing bundled debug files"
+Remove-Item -Recurse -Path $toolsDir\Debug
+
+Write-Host "Adding cert to trusted store"
+certutil -addstore -f "TrustedPublisher" $toolsDir\MarkHarmstone.cer
+
+Write-Host "Adding btrfs driver"
+pnputil -i -a $toolsDir\btrfs.inf 
+
+if (Get-OSArchitectureWidth 64) {
+	Write-Host "Removing x32 files"
+	Remove-Item -Recurse -Path $toolsDir\x86
+} else {
+	Write-Host "Removing x64 files"
+	Remove-Item -Recurse -Path $toolsDir\x64
+}
+
+#Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
 #Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
 ## If you are making your own internal packages (organizations), you can embed the installer or 
 ## put on internal file share and use the following instead (you'll need to add $file to the above)
