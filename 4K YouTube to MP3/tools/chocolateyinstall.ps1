885fc375b1322a7740142869e1b31365d731665c
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-05-25 16:47:47 -0500

    Update 4k mp3 to 3.6.2
    Former-commit-id: 84e8046aab12f5eb248dac5eb45bc5244bfdba43

--- 4K YouTube to MP3/tools/chocolateyinstall.ps1
+++ 4K YouTube to MP3/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.6.1.msi?source=chocolatey'
-$url64      = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.6.1_x64.msi?source=chocolatey'
+$url        = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.6.2.msi?source=chocolatey'
+$url64      = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.6.2_x64.msi?source=chocolatey'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = '4K Youtube to MP3*'
 
-  checksum      = '975bd0ee547cbb64e78c69efb8615ba3317d93d73ccba8d855c3ffa4fd4c5011'
+  checksum      = 'f2cd47b9317bd567246981f3940ce3ca53b2b4e86da5e7d76ca54200db03f591'
   checksumType  = 'sha256'
-  checksum64    = '302f687d0b7de51504b4bdea7de0ffc382a70b3520a50978dbe6c25cf1c8b706'
+  checksum64    = '5e3ea30e20f4a6f5637cc80c23ace1cdc37f3d98e8e2f2af3ce72e43dc5c4072'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
@@ -22,22 +22,3 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
