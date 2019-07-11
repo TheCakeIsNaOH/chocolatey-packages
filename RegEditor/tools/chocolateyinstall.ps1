a1222b3454d7fb7f68277b6d9feae896ed3a98f6
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-07-10 21:51:47 -0400

    regeditor initial

---
+++ RegEditor/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipLocation = Join-Path $toolsDir 'ooregeditor.zip'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  FileFullPath   = $zipLocation
+  Destination    = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs
