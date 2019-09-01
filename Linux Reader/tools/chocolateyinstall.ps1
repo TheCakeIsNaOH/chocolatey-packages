27c69509949f7c7e7f8d03e1cda9745959d94568
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-09-01 15:21:40 -0500

    Linux reader to 3.5

--- Linux Reader/tools/chocolateyinstall.ps1
+++ Linux Reader/tools/chocolateyinstall.ps1
@@ -1,26 +1,19 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.diskinternals.com/download/Linux_Reader.exe'
+$fileLocation = Join-Path $toolsDir 'Linux_Reader.exe'
 $desktop = [Environment]::GetFolderPath("Desktop")
 $shortcut = join-path $desktop "DiskInternals Research.lnk"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
-
-  softwareName  = 'Linux Reader*'
-
-  checksum      = 'b2b9ce715a0f50edc10afcc6446f3ff1bb9f6117a2c38f628a3c6deedc26de31'
-  checksumType  = 'sha256'
-
+  file 			= $fileLocation
   silentArgs   = '/S'
   validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
 Write-Host "removing " $shortcut
 Remove-item $shortcut
