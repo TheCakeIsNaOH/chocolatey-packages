0e3c40ae7de95e60d381d07a4bef732d008302b3
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-09-06 21:52:17 -0500

    libdvdcss-2 initial

---
+++ libdvdcss-2/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   		   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$sys32 				   = [System.Environment]::SystemDirectory
+$dll64 				   = join-path $toolsDir 'x64\libdvdcss-2.dll'
+$dll32 				   = join-path $toolsDir 'x32\libdvdcss-2.dll'
+
+Write-Host -ForegroundColor green 'Copying libdvdcss-2.dll to system32'
+if (Get-OSArchitectureWidth 64) {
+Copy-Item $dll64 -Destination $sys32
+} else {
+Copy-Item $dll32 -Destination $sys32
+}
---
+++ libdvdcss-2/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop';
+$sys32 = [System.Environment]::SystemDirectory
+$dll = Join-Path $sys32 'libdvdcss-2.dll'
+
+Write-Host -ForegroundColor green 'Removing libdvdcss-2.dll from system32'
+Remove-Item -Path $dll
