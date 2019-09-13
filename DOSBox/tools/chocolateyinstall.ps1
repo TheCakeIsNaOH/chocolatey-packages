9ad6c557d931e3c0c85ce3277d89389071166610
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-09-13 12:34:03 -0500

    DOSBox intial

---
+++ DOSBox/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation          = Join-Path $toolsDir 'DOSBox-win32-installer.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file          = $fileLocation
+  silentArgs    = '/S'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
