c506844ba2d3fce7f7feff9c7e8b839bc35ae649
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-09-19 17:32:21 -0500

    4k video to 4.9.2

--- 4K Video Downloader/tools/chocolateyinstall.ps1
+++ 4K Video Downloader/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.4kdownload.com/app/4kvideodownloader_4.9.0.msi?source=chocolatey'
-$url64      = 'https://dl.4kdownload.com/app/4kvideodownloader_4.9.0_x64.msi?source=chocolatey'
+$url        = 'https://dl.4kdownload.com/app/4kvideodownloader_4.9.2.msi?source=chocolatey'
+$url64      = 'https://dl.4kdownload.com/app/4kvideodownloader_4.9.2_x64.msi?source=chocolatey'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = '4K Video Downloader*'
 
-  checksum      = '46aed6ef255afafafa795f6aeb14c03bf8c03fe8e352f504048ecb273bac664f'
+  checksum      = 'b01d6a1418232dcdd2fd755f4c61a2a77fb4d15c658d791fdf30b841fadea3d7'
   checksumType  = 'sha256'
-  checksum64    = '2dc0f5b5c4ff8d9f9b3baa5005b513251f7ea2a640bf56fb9b7f3335457ddc64'
+  checksum64    = '5c2f22789e26ff10a754c2e59a6bb1632703f13c1ac3ebc7f009807d6524e7b5'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
