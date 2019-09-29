14dce003625ce2567ff02df82ea40c85ed763b06
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-09-28 21:19:53 -0500

    Balabolka add noicon but no version change

--- Balabolka/tools/chocolateyinstall.ps1
+++ Balabolka/tools/chocolateyinstall.ps1
@@ -1,7 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'setup.exe'
-
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation          = Join-Path $toolsDir 'setup.exe'
+$pp                    = Get-PackageParameters
+$shortcutName          = 'Balabolka.lnk'
+$shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,3 +15,12 @@ $packageArgs = @{
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+
+if ($pp['noicon']) {
+	if (Test-Path $shortcut) {
+		Remove-Item $shortcut
+		Write-Host -ForegroundColor green 'Removed ' $shortcut
+	} else {
+		Write-Host -ForegroundColor yellow 'Did not find ' $shortcut
+	}
+}
