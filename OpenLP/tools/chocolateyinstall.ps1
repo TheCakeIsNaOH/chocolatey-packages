cc3ce4f965c6a0f7940ce95e5d074fd1693e780f
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-10-04 12:13:08 -0500

    OpenLP internalize and addnoicon

--- OpenLP/tools/chocolateyinstall.ps1
+++ OpenLP/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$Url = 'https://get.openlp.org/2.4.6/OpenLP-2.4.6-setup.exe'
+$fileLocation          = Join-Path $toolsDir 'OpenLP-setup.exe'
 $pp                    = Get-PackageParameters
 $shortcutName          = 'OpenLP.lnk'
 $shortcut              = Join-Path ([Environment]::GetFolderPath("CommonDesktopDirectory")) $shortcutName
@@ -10,14 +10,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  Url 			= $Url
+  file          = $fileLocation
   softwareName  = 'OpenLP*'
-  checksum      = '954326ec4d9bd8ce060003889934879cb3c93a7965840a7e9464e87a4cb5f74c'
-  checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
 if ($pp['noicon']) {
 	if (Test-Path $shortcut) {
