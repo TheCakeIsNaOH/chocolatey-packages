c6988f62cbc8c113a4713ff8513c257af82501bb
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-10-19 21:28:01 -0500

    4k-video to mp 3 initial

---
+++ 4k-video-to-mp3/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32                 = 'https://dl.4kdownload.com/app/4kvideotomp3_1.7.1.msi?source=chocolatey'
+$url64                 = 'https://dl.4kdownload.com/app/4kvideotomp3_1.7.1_x64.msi?source=chocolatey'
+$pp                    = Get-PackageParameters
+$shortcutName          = '4K Video to MP3.lnk'
+$shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
+$checksum32            = 'c1f82ba687632fd615904b0ac8d3a4d6679fe4da3fb818cc52a807c7967307c9'
+$checksum64            = 'dcac4b38e766458a67d9184d0607c45f3e06c95f9259e7eb541134bd2422982c'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url32
+  url64bit      = $url64
+
+  softwareName  = '4K Video to MP3*'
+
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+if ($pp['noicon']) {
+	if (Test-Path $shortcut) {
+		Remove-Item $shortcut
+		Write-Host -ForegroundColor green 'Removed ' $shortcut
+	} else {
+		Write-Host -ForegroundColor yellow 'Did not find ' $shortcut
+	}
+}
+
