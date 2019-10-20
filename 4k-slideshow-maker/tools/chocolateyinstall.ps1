3df399657d75364fd1c1451d367ad4fa5a6aca5f
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-10-19 21:08:38 -0500

    Create 4k slideshow

---
+++ 4k-slideshow-maker/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32                 = 'https://dl.4kdownload.com/app/4kslideshowmaker_1.7.1.msi?source=chocolatey'
+$url64                 = 'https://dl.4kdownload.com/app/4kslideshowmaker_1.7.1_x64.msi?source=chocolatey'
+$pp                    = Get-PackageParameters
+$shortcutName          = '4K Slideshow Maker.lnk'
+$shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
+$checksum32            =  '3ec5b57eb15f9faf24e302249e4ad1c97e008dea0f2399819f4c54ddd077d0c8'
+$checksum64            =  'd1848ce05ea5e04849797bde93da843db446f8358f3a8208ca88a0aa1900cc79'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url32
+  url64bit      = $url64
+
+  softwareName  = '4K Slideshow Maker*'
+
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+if ($pp['noicon']) {
+	if (Test-Path $shortcut) {
+		Remove-Item $shortcut
+		Write-Host -ForegroundColor green 'Removed ' $shortcut
+	} else {
+		Write-Host -ForegroundColor yellow 'Did not find ' $shortcut
+	}
+}
+
---
+++ 4k-slideshow-maker/update.ps1
@@ -0,0 +1,31 @@
+Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"          
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"          
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+		}
+	}
+}
+
+
+function global:au_GetLatest {
+	$download_page = Invoke-WebRequest -Uri https://www.4kdownload.com/download -UseBasicParsing
+	
+	$regex64       = '4kslideshowmaker_.*_x64.msi'
+	$regex32       = '4kslideshowmaker_.*\.msi'
+    $url64         = $download_page.links | ? href -match $regex64 | select -First 1 -expand href
+	$url32         = $download_page.links | ? href -match $regex32 | select -First 1 -expand href 
+	
+	$version       = $url64 -split '[_]' | select -Last 1 -Skip 1
+	
+	$modurl64      = $url64 -replace 'website', 'chocolatey'
+	$modurl32      = $url32 -replace 'website', 'chocolatey'
+	
+	return @{ Version = $version; URL32 = $modurl32; URL64 = $modurl64 }
+}
+
+update
