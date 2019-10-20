55d73a82b6f4cd3c8c57d8b37bf24e374717c50e
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-10-19 20:31:50 -0500

    Add 4k stogram

---
+++ 4k-stogram/tools/chocolateyinstall.ps1
@@ -0,0 +1,40 @@
+$ErrorActionPreference = 'Stop';
+$packageName		   = '4K Stogram'
+$toolsDir  			   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32                 = 'https://dl.4kdownload.com/app/4kstogram_2.7.3.msi?source=chocolatey'
+$url64                 = 'https://dl.4kdownload.com/app/4kstogram_2.7.3_x64.msi?source=chocolatey'
+$checksum32            = '22800ee03f54d249ef5eed1fa4298345b42b09e5efa98b4d2e7473465bb0f341'
+$checksum64            = '21d47632a2222ee92dd8ad2e14a1d4544335a800900e3324313a55ad1acf5a24'
+$pp                    = Get-PackageParameters
+$shortcutName          = '4K Stogram.lnk'
+$shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url32
+  url64bit      = $url64
+
+  softwareName  = '4K Stogram*'
+
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+if ($pp['noicon']) {
+	if (Test-Path $shortcut) {
+		Remove-Item $shortcut
+		Write-Host -ForegroundColor green 'Removed ' $shortcut
+	} else {
+		Write-Host -ForegroundColor yellow 'Did not find ' $shortcut
+	}
+}
