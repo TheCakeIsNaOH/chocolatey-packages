c7a79e936fe5a87a292c73bd003cc19d72b840aa
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-10-24 15:41:34 -0500

    Add zfsin

---
+++ zfsin/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation          = Join-Path $toolsDir 'OpenZFSOnWindows-release.exe'
+$cerLocation           = Join-Path $toolsDir 'ZFSin.cer'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file64        = $fileLocation
+  softwareName  = 'OpenZFS On Windows'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0) 
+}
+
+certutil -addstore -f "TrustedPublisher" $cerLocation
+
+Install-ChocolateyInstallPackage @packageArgs
