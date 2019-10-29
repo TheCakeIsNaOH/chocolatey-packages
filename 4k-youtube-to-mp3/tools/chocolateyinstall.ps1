51d66d561488e38f71352956aeed0ef078378d22
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-10-28 20:19:28 -0500

    4k youtube to 3.8.3

--- 4k-youtube-to-mp3/tools/chocolateyinstall.ps1
+++ 4k-youtube-to-mp3/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.8.2.msi?source=chocolatey'
-$url64                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.8.2_x64.msi?source=chocolatey'
-$checksum32            = '9fb5f9355f660140d175cfa38f45c113e521c37ca6593170e856cfd2cc7e978b'
-$checksum64            = '4e34f428f83a76bf79c8121a40e377ddbd58d26187fce705af32427c93f6d359'
+$url32                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.8.3.msi?source=chocolatey'
+$url64                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.8.3_x64.msi?source=chocolatey'
+$checksum32            = 'fc93e1e2e09b7238aa987d61a6e8b2d567857b588c6cb4f66a8d0fb4f6b802bb'
+$checksum64            = '54be34245aeafb8baea76435dd94824c47b157260cfdd85d788e3e2b8eccda95'
 $pp                    = Get-PackageParameters
 $shortcutName          = '4K YouTube to MP3.lnk'
 $shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
