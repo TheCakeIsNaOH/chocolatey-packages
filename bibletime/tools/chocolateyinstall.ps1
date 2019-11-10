aeb5a9706794a53c28344d3af525bcd7edd97610
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-11-09 21:19:05 -0600

    Bibletime add automatic updates

--- bibletime/tools/chocolateyinstall.ps1
+++ bibletime/tools/chocolateyinstall.ps1
@@ -1,40 +1,15 @@
-﻿
-$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'BibleTime-2.11.2-win32.exe'
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName              = (Get-ChildItem $toolsDir -Filter "*.exe")
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'EXE'
-  file         = $fileLocation
-
+  file          = (Join-Path $toolsDir $fileName)
   softwareName  = 'BibleTime*'
-
-  checksum      = 'ccb2018f0e892bc57397f3056cde6ed950790cf31ad1adafaee102c26b1d0212'
-  checksumType  = 'sha256'
-
-  silentArgs   = '/S'
+  silentArgs    = '/S'
   validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
+Install-ChocolateyInstallPackage @packageArgs
 
---
+++ bibletime/update.ps1
@@ -0,0 +1,25 @@
+Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+		}
+	}
+}
+
+function global:au_BeforeUpdate() {
+	Get-RemoteFiles -Purge -NoSuffix 
+}
+
+
+function global:au_GetLatest {
+	$download_page = Invoke-WebRequest -Uri https://github.com/bibletime/bibletime/releases -UseBasicParsing
+	
+	$url        = $download_page.links | ? href -match '.exe$'| % href | select -First 1
+	$version    = $url -split '[-]' | select -Last 1 -Skip 1
+	$modurl     = 'https://github.com' + $url 
+	
+	return @{ Version = $version; URL32 = $modurl; PackageName = 'bibletime'}
+}
+
+update  -ChecksumFor none
