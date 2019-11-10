a66acdef4002783e2e3288d8f70baea153ddc9b7
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-11-10 08:21:03 -0600

    Attempt to prevent debug package downloads instead of release

--- WinBtrfs/update.ps1
+++ WinBtrfs/update.ps1
@@ -15,7 +15,7 @@ function global:au_BeforeUpdate() {
 function global:au_GetLatest {
 	$download_page = Invoke-WebRequest -Uri https://github.com/maharmstone/btrfs/releases -UseBasicParsing
 	
-	$url        = $download_page.links | ? href -match '.zip$'| % href | select -First 1
+	$url        = $download_page.links | ? href -match 'btrfs-\d\.\d[\d\.]*\.zip$' | % href | select -First 1
 	$version    = ($url -split '[/]' | select -Last 1 -Skip 1).substring(1)
 	$modurl     = 'https://github.com' + $url 
 
