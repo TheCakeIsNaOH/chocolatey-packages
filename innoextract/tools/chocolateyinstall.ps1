68abd73ea240b21b2992d16fa280acc489a08154
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-11-11 09:08:17 -0600

    Innoextract initial

---
+++ innoextract/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation          = (Get-ChildItem $toolsDir -Filter "*.zip").FullName
+
+Get-ChocolateyUnzip $fileLocation $toolsDir
+
