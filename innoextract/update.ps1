89210f43b3c37de913ac5c549ca39a9dbfb33ba1
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-11-11 20:15:34 -0600

    innoextract add AU

---
+++ innoextract/update.ps1
@@ -0,0 +1,25 @@
+Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+		}
+	}
+}
+
+function global:au_BeforeUpdate() {
+	Get-RemoteFiles -Purge -NoSuffix 
+}
+
+
+function global:au_GetLatest {
+	$download_page = Invoke-WebRequest -Uri https://constexpr.org/innoextract/files/ -UseBasicParsing
+	
+	$full_version  = $download_page.links | ? href -match 'innoextract-\d\.\d' | % href | select -last 1
+	$version    = ($full_version -split '-' | select -Last 1).trim('/')
+	$url        = 'https://constexpr.org/innoextract/files/innoextract-' + $version + '/innoextract-' + $version + '-windows.zip'
+
+	return @{ Version = $version; URL32 = $url; PackageName = 'innoextract'}
+}
+
+update  -ChecksumFor none
