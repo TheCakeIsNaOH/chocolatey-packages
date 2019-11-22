8da14483194712a730b82c10a1e49ee27226fa2e
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2019-11-22 15:54:50 -0600

    Reflect free try to update

--- reflect-free/tools/chocolateyInstall.ps1
+++ reflect-free/tools/chocolateyInstall.ps1
@@ -1,23 +1,46 @@
-﻿$fileName     = 'ReflectDL.exe'
-$softwareName = 'Macrium Reflect Installer'
-$toolsPath    = "$env:systemdrive\tools"
-$filePath     = "$toolsPath\$fileName"
-$packageArgs  = @{
-  packageName             = 'reflect-free'
-  url                     = 'http://updates.macrium.com/reflect/v6/ReflectDL.exe'
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = Split-Path $MyInvocation.MyCommand.Definition
+$url                   = 'https://updates.macrium.com/reflect/v7/ReflectDLHF.exe'
+$agentfileName         = $url -split '/' | select -Last 1
+$downloadDir           = (Join-Path $(Get-ToolsLocation) "reflect-free")
+
+
+if ((Get-WmiObject win32_operatingsystem).caption -match "Server") {
+	Write-Host -ForegroundColor red "Non compatible Windows Server OS detected"
+	throw  "Install failed due to Server OS"
 }
 
-Get-WebFile "$($packageArgs.url)" "$filePath"
+Remove-Item $downloadDir -Recurse -ea 0
 
-# Shortcut
-$desktop = [System.Environment]::GetFolderPath('Desktop')
-$startMenu = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::StartMenu)
-Install-ChocolateyShortcut -ShortcutFilePath "$desktop\$softwareName.lnk" -TargetPath "$filePath"
-Install-ChocolateyShortcut -ShortcutFilePath "$startMenu\Programs\$softwareName.lnk" -TargetPath "$filePath"
+$downloadArgs = @{
+	packageName   = 'reflect-free'
+	FileFullPath  = (Join-Path $downloadDir $agentfileName)
+	url           = $url
+	forceDownload = $true
+}
 
-# Warn about unfinished business
-Write-Host
-Write-Host "The installer could not be automated." -ForegroundColor "White"
-Write-Host "We've placed a link to the installer on your Desktop." -ForegroundColor "White"
-Write-Host "You can manually run this installer at your earliest convenience." -ForegroundColor "White"
-Write-Host
+Get-ChocolateyWebFile @downloadArgs
+
+Write-Host -ForegroundColor green "Running Macrium download agent via Autohotkey"
+Autohotkey.exe $toolsDir\download.ahk "$downloadDir\$agentfileName" $downloadDir
+
+$installer = Get-Item $downloadDir\Macrium\*.exe -ea 0
+
+if (!$installer) { 
+	Write-Host -ForegroundColor red "Autohotkey script failed for Macrium download agent, please run manually $downloadDir\$agentfileName" 
+}
+else {
+	$packageArgs = @{
+		packageName    = 'reflect-free'
+		fileType       = 'exe'
+		file           = $installer
+		validExitCodes = @(0)
+		silentArgs     = "/qn /norestart"
+	}
+
+	Write-Host -ForegroundColor green "Running $installer"
+	Install-ChocolateyInstallPackage @packageArgs
+
+	Write-Host -ForegroundColor green "Installation completed"
+	Write-Host -ForegroundColor green "Downloaded files are left in: $downloadDir"
+}
---
+++ reflect-free/tools/download.ahk
@@ -0,0 +1,46 @@
+;exe_path   = exepath
+;setup_dir  = path
+
+exe_path   = %1%
+setup_dir  = %2%
+
+Run, %exe_path%
+
+WinWait, Macrium Reflect Download Agent
+hwnd := WinExist()
+
+; Set download directory
+ControlSetText, Edit2, %setup_dir%, ahk_id %hwnd%
+Sleep 500
+
+; Do not run after download
+ControlClick, Button6, ahk_id %hwnd%
+
+Sleep 3000
+
+; Download
+ControlClick, Button10, ahk_id %hwnd%
+Sleep 100
+Send, {Enter}
+
+; Accept overwrite old installer if needed
+Sleep 1500
+IfWinExist,,&Yes
+{
+    WinActivate
+    Send, {ENTER}
+}
+
+
+; Exit after done downloading
+loop {
+    Sleep 1000
+    IfWinExist,, Click to view log file
+    {
+        WinActivate
+        Send, {ENTER}
+        ControlSend,,{ESC}, ahk_id %hwnd% 
+        break
+    }
+}
+
