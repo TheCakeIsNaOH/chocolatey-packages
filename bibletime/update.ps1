71dc5e1d2fc8a51ead55200ceddc7dc1729e9595
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2019-12-13 11:58:32 -0600

    bibletime re-enable AU

--- bibletime/update.ps1
+++ bibletime/update.ps1
@@ -16,7 +16,7 @@ function global:au_GetLatest {
 	$download_page = Invoke-WebRequest -Uri https://github.com/bibletime/bibletime/releases -UseBasicParsing
 	
 	$url        = $download_page.links | ? href -match '.exe$'| % href | select -First 1
-	$version    = $url -split '[-]' | select -Last 1 -Skip 1
+	$version    = ($url -split '[-]' | select -Last 1 -Skip 1).replace('_','-')
 	$modurl     = 'https://github.com' + $url 
 	
 	return @{ Version = $version; URL32 = $modurl; PackageName = 'bibletime'}
