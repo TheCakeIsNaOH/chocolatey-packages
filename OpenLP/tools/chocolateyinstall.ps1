02b6f4ceb1b9f0f0197e58e501884a4b21c2de97
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2019-12-27 11:24:18 -0600

    OpenLP prepare for 3.0

--- OpenLP/tools/chocolateyinstall.ps1
+++ OpenLP/tools/chocolateyinstall.ps1
@@ -1,27 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation          = Join-Path $toolsDir 'OpenLP-setup.exe'
-$pp                    = Get-PackageParameters
-$shortcutName          = 'OpenLP.lnk'
-$shortcut              = Join-Path ([Environment]::GetFolderPath("CommonDesktopDirectory")) $shortcutName
+$url32                 = 'https://get.openlp.org/2.9.0/OpenLP-2.9.0-x64.msi'
+$url64                 = 'https://get.openlp.org/2.9.0/OpenLP-2.9.0.msi'
 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  file          = $fileLocation
   softwareName  = 'OpenLP*'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url           = $url32
+  url64bit      = $url64
+  FileType      = 'MSI'
+  checksum      = '13975bec5adb4f1bf051801ac3a0ba332f270368c48e75a8d68d613b655223c9'
+  checksumType  = 'sha256' 
+  checksum64    = 'f80180ee0e0e52249e8bb12f8f88364631b5b935959e643037a64a428fa1f33d'
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyInstallPackage @packageArgs
-
-if ($pp['noicon']) {
-	if (Test-Path $shortcut) {
-		Remove-Item $shortcut
-		Write-Host -ForegroundColor green 'Removed ' $shortcut
-	} else {
-		Write-Host -ForegroundColor yellow 'Did not find ' $shortcut
-	}
-}
+Install-ChocolateyPackage @packageArgs
