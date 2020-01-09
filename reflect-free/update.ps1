c6b885a546c9d2d756eab249022f1c9b564fef7a
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-01-09 16:32:11 -0600

    reflect add back AU update

---
+++ reflect-free/update.ps1
@@ -0,0 +1,27 @@
+﻿import-module au
+
+function global:au_SearchReplace {
+  @{
+    "tools\chocolateyInstall.ps1" = @{
+      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+	  "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }	
+  }
+}
+
+function global:au_GetLatest {
+  $latest_changelog  = 'http://updates.macrium.com/reflect/v7/latest_release_notes.asp'
+  $download_page     = Invoke-WebRequest -Uri $latest_changelog -UseBasicParsing -Method Head
+  
+  $current_changelog = ($download_page.BaseResponse.ResponseUri.AbsoluteUri) + ($download_page.BaseResponse.RequestMessage.RequestUri.AbsoluteUri)
+  
+  $version           = ($current_changelog.tostring() -split '/' | select -Last 1 -Skip 1).trimstart('v')
+
+  @{
+	PackageName     = 'reflect-free'
+    Version         = "$version"
+    URL32           = 'https://updates.macrium.com/reflect/v7/ReflectDLHF.exe'
+  }
+}
+
+Update-Package
