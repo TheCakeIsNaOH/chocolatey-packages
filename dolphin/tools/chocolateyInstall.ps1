6c13fa945c2dfb1d3f1c246ef53e62abfb88fe91
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-01-09 17:08:49 -0600

    Dolphin add package parameters

--- dolphin/tools/chocolateyInstall.ps1
+++ dolphin/tools/chocolateyInstall.ps1
@@ -1,17 +1,32 @@
-﻿$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'sha256'
-$url64 = $url
-$checksum64 = $checksum
-$checksumType64 = $checksumType
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir 			   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pp                    = Get-PackageParameters
+$shortcutName          = 'Dolphin Emulator.lnk'
+$extractDir            = (Join-Path $(Get-ToolsLocation) Dolphin)
+$exepath               = (Join-Path $extractDir Dolphin.exe)
+$url64                 = 'https://dl.dolphin-emu.org/builds/40/18/dolphin-master-5.0-11452-x64.7z'
+$checksum64            = '1734b6690d43270e02e110b5b1e9d2ab9660dbebbca8b01673edbebec3dcfce9'
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Url64bit "$url64" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
-                             -Checksum64 "$checksum64" `
-                             -ChecksumType64 "$checksumType64"
+$packageArgs = @{
+  Url64bit       = $url64
+  Checksum64     = $checksum64
+  ChecksumType64 = 'sha256'
+  UnzipLocation  = $extractDir
+  PackageName    = 'dolphin'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-BinFile -Name 'Dolphin' -Path $exepath -UseStart
+
+if ($pp['desktopicon']) {
+	$desktopicon = (Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName)
+	Write-Host -ForegroundColor green 'Adding ' $desktopicon
+	Install-ChocolateyShortcut -ShortcutFilePath $desktopicon -TargetPath $exepath  -RunAsAdmin
+}
+
+if (!$pp['nostart']) {
+	$starticon = (Join-Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) $shortcutName)
+	Write-Host -ForegroundColor green 'Adding ' $starticon
+	Install-ChocolateyShortcut -ShortcutFilePath $starticon -TargetPath $exepath  -RunAsAdmin
+}
---
+++ dolphin/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir 			   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$extractDir            = (Join-Path $(Get-ToolsLocation) Dolphin)
+$exepath               = (Join-Path $extractDir Dolphin.exe)
+$shortcutName          = 'Dolphin.exe'
+$desktopicon           = (Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName)
+$starticon             = (Join-Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) $shortcutName)
+
+
+if (Test-Path $desktopicon) {
+	Remove-Item $desktopicon
+	Write-Host -ForegroundColor green 'Removed ' $desktopicon
+} else {
+	Write-Host -ForegroundColor yellow 'Did not find ' $desktopicon
+}
+
+if (Test-Path $starticon) {
+	Remove-Item $starticon
+	Write-Host -ForegroundColor green 'Removed ' $starticon
+} else {
+	Write-Host -ForegroundColor yellow 'Did not find ' $starticon ' to remove'
+}
+
+Uninstall-BinFile -Name 'Dolphin' -Path $exepath
+
+
+#delete files? files included and some folders?
