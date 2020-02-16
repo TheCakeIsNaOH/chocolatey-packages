ddb908d3f851b0ab4cc1ee17ff76ffbb274dcebb
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-02-15 18:22:31 -0600

    fastest mouse try to fix AU

--- The Fastest Mouse Clicker for Windows/update.ps1
+++ The Fastest Mouse Clicker for Windows/update.ps1
@@ -8,18 +8,18 @@ function global:au_SearchReplace {
 }
 
 function global:au_BeforeUpdate() {
-	Get-RemoteFiles -Purge -NoSuffix 
+	Get-RemoteFiles -Purge -NoSuffix -FileNameSkip 1
 }
 
 
 function global:au_GetLatest {
 	$download_page = Invoke-WebRequest -Uri https://sourceforge.net/projects/fast-mouse-clicker-pro/files/ -UseBasicParsing
 	
-	$url        = $download_page.links | ? href -match 'https://sourceforge.net.*nstall.*\.exe' | select -first 1 -ExpandProperty href
-	$exename    = $url -split '[/]' | select -Last 1 -Skip 1
+	$url32        = $download_page.links | ? href -match 'https://sourceforge.net.*nstall.*\.exe' | select -first 1 -ExpandProperty href
+	$exename    = $url32 -split '[/]' | select -Last 1 -Skip 1
     $version    = ($exename -split '[a-z]' | ? { $_ -match '\d'}).replace("_" , ".").trim(".")
 
-	return @{ Version = $version; URL32 = $url; PackageName = 'fastest-mouse-clicker'}
+	return @{ Version = $version; URL32 = $url32; PackageName = 'fastest-mouse-clicker'; FileType = 'exe'}
 }
 
-Update-Package -ChecksumFor none -NoCheckUrl
+Update-Package -ChecksumFor none -nocheckurl
