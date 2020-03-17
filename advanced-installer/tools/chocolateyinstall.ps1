bb2bdc8b0a27ed09a3bbc4562962186c51240f27
Chocolatey <chocolatey@realdimensions.net>
2020-03-17 21:02:33 +0000

    AU: 1 updated - advanced-installer [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/01a59b176b8b48d55e5ff79a65cefd32fdab2d05

--- advanced-installer/tools/chocolateyinstall.ps1
+++ advanced-installer/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url                   = 'https://www.advancedinstaller.com/downloads/16.8/advinst.msi'
-$checksum              = 'f1f01b1c10b44b3ccc67ff897205d346c3dfb458036f4d61552262ac3aea32c7'
+$url                   = 'https://www.advancedinstaller.com/downloads/16.9/advinst.msi'
+$checksum              = '4076e6d241e93a91c1a697a9ae7d1baaea41ce2264dd3b8fa9c194700eeaa391'
 
 $packageArgs = @{
 	packageName    = 'advanced-installer'
