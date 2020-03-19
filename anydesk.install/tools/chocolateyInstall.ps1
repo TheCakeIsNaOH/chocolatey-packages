33f8e919a1b0e871ee2991d3034aaebdeb9ab814
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-03-19 09:58:01 -0500

    Add anydesk.install

---
+++ anydesk.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'Stop'
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32                 = 'https://download.anydesk.com/AnyDesk.msi'
+$checksum32            = '3EEFBBF3417E4C4582BF646C55541D6D8E37750DB44EE11D7745CBFD07A4AA67'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url32
+  softwareName  = 'AnyDesk MSI'
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
