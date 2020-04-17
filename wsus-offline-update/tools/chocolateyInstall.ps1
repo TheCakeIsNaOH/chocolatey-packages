151e054dd78797fadbd12f90126c20fe7fb00231
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-04-16 21:59:48 -0500

    Add wsus files

---
+++ wsus-offline-update/tools/chocolateyInstall.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pp                    = Get-PackageParameters
+$file                  = (Get-Childitem -Filter *.zip -Path $toolsDir).fullname
+$destDir               = $toolsDir
+
+if ($pp['path']) {
+	if (!(Test-Path $pp['path'])) {
+		Throw "Invalid path specified"
+	}
+	$destDir = $pp['path']
+}
+
+$packageArgs = @{
+  FullFilePath = $file
+  Destination  = $destDir
+  PackageName  = $env:ChocolateyPackageName
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+if (!$pp['nostart']) {
+	$fileNameGen  = "UpdateGenerator.exe"
+	$fileNameInst = "UpdateInstaller.exe"
+	$linkNameGen  = "UpdateGenerator.lnk"
+	$linkNameInst = "UpdateInstaller.lnk"
+	$programs     = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+	$shortcutFilePathGen  = Join-Path $programs $linkNameGen
+	$shortcutFilePathInst = Join-Path $programs $linkNameInst
+	$targetPathGen        = [System.IO.Path]::Combine("$destdir", "wsusoffline", $fileNameGen)
+	$targetPathInst       = [System.IO.Path]::Combine("$destdir", "wsusoffline", "client", $fileNameInst)
+	Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathGen -targetPath $targetPathGen
+	Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathInst -targetPath $targetPathInst
+}
+
+Remove-Item $file -ea 0
---
+++ wsus-offline-update/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$linkNameGen           = "UpdateGenerator.lnk"
+$linkNameInst          = "UpdateInstaller.lnk"
+$startmenu             = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePathGen   = Join-Path $startmenu $linkNameGen
+$shortcutFilePathInst  = Join-Path $startmenu $linkNameInst
+
+if(Test-Path $shortcutFilePathGen) {
+    del $shortcutFilePathGen
+}
+
+if(Test-Path $shortcutFilePathInst) {
+    del $shortcutFilePathInst
+}
+
+Write-Host -ForegroundColor yellow "Note that if you installed the software to a custom path then it is not removed"
