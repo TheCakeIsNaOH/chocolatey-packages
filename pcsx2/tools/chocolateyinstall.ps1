33d0545d2dfd88c32a514b70769acfc6c2ff3971
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-05-30 12:36:25 -0500

    PCSX2 first files

---
+++ pcsx2/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file          = $fileLocation
+  softwareName  = 'PCSX2*'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
