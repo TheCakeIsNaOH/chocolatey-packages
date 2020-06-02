3a7e9081a47c7d77b4bd0d1c643221aa138e8df2
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-06-02 12:32:30 -0500

    PCSX2.portable add 1.6.0

---
+++ pcsx2.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,51 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation          = (Get-ChildItem -Path $toolsDir -Filter "pcsx2*.7z").fullname
+$pp                    = Get-PackageParameters
+$shortcutName          = 'PCSX2 Portable.lnk'
+$startPath             = Join-Path ([Environment]::GetFolderPath("CommonStartMenu")) $shortcutName
+$desktopPath           = Join-Path ([Environment]::GetFolderPath("CommonDesktop")) $shortcutName
+$tempPath              = Join-Path $env:temp 'PCSX2-portable'
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  FileFullPath  = $fileLocation
+  Destination   = "$tempPath"
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+if ($pp['Path']) {
+	$destination = $pp['Path']
+	if (!(Test-Path "$destination" -IsValid)) {
+		Throw "Bad path parameter"
+	}
+} else {
+	$destination = Join-Path $(Get-ToolsLocation) 'PCSX2'
+}
+
+Mkdir -Path $destination -Force | Out-Null
+
+$configFile = [IO.Path]::Combine("$destination" , "Shaders" , "GSdx_FX_Settings.ini")
+if (Test-Path $configFile) {
+	Write-Output "Backing up GSdx_FX_Settings.ini shaders folder"
+	Rename-Item -Path $configFile -Force -ea 0 -NewName ("GSdx_FX_Settings-" + (Get-Date -Format "yyyy-MM-dd").tostring() + "-backup.ini")
+}
+
+$innerFolder = (Get-ChildItem -Path "$tempPath" -Filter "PCSX2*").FullName
+$fileList = Get-ChildItem -Path $innerFolder | Copy-Item -Destination $destination -Recurse -Force -PassThru
+$fileList | select -ExpandProperty FullName | Out-File -Force -FilePath (Join-Path $toolsDir 'install-files.txt')
+
+$exePath = Join-Path "$destination" "pcsx2.exe"
+
+if ($pp['Start']) {
+	Install-ChocolateyShortcut -ShortcutFilePath "$startPath" -TargetPath "$exePath" -WorkingDirectory "$destination"
+}
+
+if ($pp['Desktop']) {
+	Install-ChocolateyShortcut -ShortcutFilePath "$desktopPath" -TargetPath "$exePath" -WorkingDirectory "$destination"
+}
+
+Remove-Item -ea 0 -Path $fileLocation
+Remove-Item -Recurse -ea 0 -Path $tempPath 
---
+++ pcsx2.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$shortcutName          = 'PCSX2 Portable.lnk'
+$startPath             = Join-Path ([Environment]::GetFolderPath("CommonStartMenu")) $shortcutName
+$desktopPath           = Join-Path ([Environment]::GetFolderPath("CommonDesktop")) $shortcutName
+$installpathfile       = Join-Path $toolsDir 'install-files.txt'
+
+
+Remove-Item -ea 0 -Force -Path $startPath 
+Remove-Item -ea 0 -Force -Path $desktopPath 
+
+If (Test-Path $installpathfile) {
+	$fileList = Get-Content -Path $installpathfile
+	$fileList | Where-Object { $_ -NotMatch "Plugins|Shaders" } | Remove-Item -Recurse -Force -ea 0
+	Write-Output "Configuration and other user created files left at install location"
+	Write-Output "Default install location is in Chocolatey tools directory, defaults to C:\tools\pcsx2"
+} else {
+	Write-Warning "Cannot find files to uninstall, they may need to removed manually"
+	Write-Warning "Default install location is in Chocolatey tools directory, defaults to C:\tools\pcsx2"
+}
+
