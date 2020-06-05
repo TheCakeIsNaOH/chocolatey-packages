13bfb7afdb3bd6e9ebf9f1c214aac6bb09b300fb
Chocolatey <chocolatey@realdimensions.net>
2020-06-05 21:01:01 +0000

    AU: 1 updated - 4k-youtube-to-mp3 [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/7a51bada2263893b7a5742f078f7b36921651b8b

--- 4k-youtube-to-mp3/tools/chocolateyinstall.ps1
+++ 4k-youtube-to-mp3/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.12.2.msi?source=chocolatey'
-$url64                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.12.2_x64.msi?source=chocolatey'
-$checksum32            = 'ccea462a62d256cda54c988447d541ad8ff1ea22b3c7e3f861c613552519092d'
-$checksum64            = '7d6814e56f6a46cc6cb918f8867ef2e7f166010757be6ea5fdbe2c2037c231c3'
+$url32                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.12.3.msi?source=chocolatey'
+$url64                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.12.3_x64.msi?source=chocolatey'
+$checksum32            = 'bbcff8f2f3180e4bebee1dd961306830343d1af634cb2a70a82eea7dc7346576'
+$checksum64            = 'f21f76aa3bab0a4eb044a7fe05a1043e8853b9df5550442eb030e4650a894ae0'
 $pp                    = Get-PackageParameters
 $shortcutName          = '4K YouTube to MP3.lnk'
 $shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
