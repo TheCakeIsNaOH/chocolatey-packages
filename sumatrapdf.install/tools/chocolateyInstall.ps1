e5cbc8b48e8b2a1cf9e4a4b6d25ba4450e304a78
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-06-12 10:03:17 -0500

    Sumatrapdf install add initial

---
+++ sumatrapdf.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$files32               = (Get-ChildItem -Filter "*_x32.exe").FullName
+$files64               = (Get-ChildItem -Filter "*_x64.exe").FullName
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file32 		= $toolsDir\$file32
+  file64		= $toolsDir\$file64
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
