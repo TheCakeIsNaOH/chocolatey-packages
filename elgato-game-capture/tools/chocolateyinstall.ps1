2fc3eb2ca7515201b05b4127bb6f24486608c9de
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-06-18 11:55:44 -0500

    elgato game capture initial

---
+++ elgato-game-capture/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://edge.elgato.com/egc/windows/egcw/3.70/final/GameCaptureSetup_3.70.43.3043_x64.msi'
+$checksum64 = '8F4A81E0E4CC794EAF1C9E33C2C8ADE8B345DE1E524369FC7E31D8DFF3F83D5E'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url64bit      = $url64
+  softwareName  = 'Elgato Game Capture HD*'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
