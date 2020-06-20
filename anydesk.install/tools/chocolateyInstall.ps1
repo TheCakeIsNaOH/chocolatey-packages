2271cfe6232fa46e31dacdfafeeb6dbdd9703210
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-06-20 12:25:55 -0500

    Fix anydesk.install upgrades

--- anydesk.install/tools/chocolateyInstall.ps1
+++ anydesk.install/tools/chocolateyInstall.ps1
@@ -15,4 +15,22 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
+Write-Host -ForegroundColor green "Trying to uninstall older versions of $packageName due to a limitation in the installer"
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+    $packageArgs['file'] = ''
+    Uninstall-ChocolateyPackage @packageArgs
+    $packageArgs['silentArgs'] = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Host -ForegroundColor green "$packageName is not installed, continuing on to install"
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+
 Install-ChocolateyPackage @packageArgs
