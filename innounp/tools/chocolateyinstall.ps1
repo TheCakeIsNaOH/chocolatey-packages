a22841c02f2a7c1ae93db782a4e54bce85e25adf
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-07-11 08:21:43 -0500

    innounp fix variable names

--- innounp/tools/chocolateyinstall.ps1
+++ innounp/tools/chocolateyinstall.ps1
@@ -1,17 +1,17 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pp                    = Get-PackageParameters
-$fileFullPath          = (Join-Path $toolsDir 'innounp.exe').fullname
+$fileFullPath          = (Join-Path $toolsDir 'innounp.exe')
 $shortcutName          = 'innounp.lnk'
 
-if ($pp['desktopicon']) {
+if ($pp['desktop']) {
 	$desktopicon = (Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName)
 	Write-Host -ForegroundColor green 'Adding ' $desktopicon
-	Install-ChocolateyShortcut -ShortcutFilePath $desktopicon -TargetPath $exepath  -RunAsAdmin
+	Install-ChocolateyShortcut -ShortcutFilePath $desktopicon -TargetPath $fileFullPath  -RunAsAdmin
 }
 
 if (!$pp['nostart']) {
 	$starticon = (Join-Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) $shortcutName)
 	Write-Host -ForegroundColor green 'Adding ' $starticon
-	Install-ChocolateyShortcut -ShortcutFilePath $starticon -TargetPath $exepath  -RunAsAdmin
+	Install-ChocolateyShortcut -ShortcutFilePath $starticon -TargetPath $fileFullPath  -RunAsAdmin
 }
