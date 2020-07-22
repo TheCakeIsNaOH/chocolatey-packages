07d7d9cbfcd29c7edd18a6b93b03ecd125ddf012
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-07-22 11:05:53 -0500

    Update minecraft details

--- minecraft/tools/chocolateyinstall.ps1
+++ minecraft/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $url                   = 'https://launcher.mojang.com/download/MinecraftInstaller.msi'
-$checksum32            =  'f61cb1adf76a45d1cde2d7ab307d08acbddd630d4d8f14962a4fc7f5d8b6ae6b'
+$checksum32            = '595614D7E4BDE7DC97147581A461B6C1F2A6035485FBB9337D7A80849D2D019E'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
