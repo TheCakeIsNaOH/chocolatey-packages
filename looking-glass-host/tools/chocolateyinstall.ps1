d9ef6c5598526ddc4ac9d872ae2f8b13fa119a3f
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-08-13 09:20:13 -0500

    Looking glass host initial

---
+++ looking-glass-host/tools/chocolateyinstall.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation          = Join-Path $toolsDir 'looking-glass-host-setup.exe'
+$pp                    = Get-PackageParameters
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file          = $fileLocation
+  softwareName  = 'Looking Glass (host)'
+  silentArgs    = '/S'      
+  validExitCodes= @(0)
+}
+
+if ($pp['desktop']) {
+    $packageArgs['silentArgs'] = $packageArgs['silentArgs'] + ' /desktop'
+}
+
+if ($pp['startmenu']) {
+    $packageArgs['silentArgs'] = $packageArgs['silentArgs'] + ' /startmenu'
+}
+
+if ($pp['noservice']) {
+    $packageArgs['silentArgs'] = $packageArgs['silentArgs'] + ' /noservice'
+}
+
+if ($pp['path']) {
+    If (!(Test-Path -IsValid $pp['path'])) {
+        Write-Error "Invalid path parameter, please check that you passed a valid path"
+    }
+    $packageArgs['silentArgs'] = $packageArgs['silentArgs'] + " /D=$($pp['path'])"
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -ea 0 $fileLocation
