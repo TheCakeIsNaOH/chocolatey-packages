cabf1cb7e2ffe568a9eab605d5cd6fa3e17df95f
Chocolatey <chocolatey@realdimensions.net>
2020-08-18 21:00:58 +0000

    AU: 1 updated - 4k-stogram [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/b78a35b50cffcc60a0b7c0d1ece4e0c36c39385c

--- 4k-stogram/tools/chocolateyinstall.ps1
+++ 4k-stogram/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageName		   = '4K Stogram'
 $toolsDir  			   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32                 = 'https://dl.4kdownload.com/app/4kstogram_3.0.5.msi?source=chocolatey'
-$url64                 = 'https://dl.4kdownload.com/app/4kstogram_3.0.5_x64.msi?source=chocolatey'
-$checksum32            = '83eb29c3f46f82e134df85a6542702624065bbf4864219ae8f286bd021f427ad'
-$checksum64            = '1a3324614edc9188192d1fcc6bbbdfe236f30929c671061a4313b8f532492147'
+$url32                 = 'https://dl.4kdownload.com/app/4kstogram_3.0.7.msi?source=chocolatey'
+$url64                 = 'https://dl.4kdownload.com/app/4kstogram_3.0.7_x64.msi?source=chocolatey'
+$checksum32            = 'eb14af9779acbd89a37939a1206e3534074be4daadbf2b2c29e1438405ab4639'
+$checksum64            = '628c8b06495953c956ee98dd6f5338a43b49974b3bf4b676074c414ff24b3189'
 $pp                    = Get-PackageParameters
 $shortcutName          = '4K Stogram.lnk'
 $shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
