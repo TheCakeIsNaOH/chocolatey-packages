d509d439c9425e8a883689b75fa9aac3891d8dda
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-09-15 12:18:45 -0500

    hack regeditor so it no longer fails, sigh

--- RegEditor/update.ps1
+++ RegEditor/update.ps1
@@ -1,13 +1,12 @@
 ﻿Import-Module AU
+Import-Module wormies-au-helpers
 
 function global:au_GetLatest {
-	$download_page = Invoke-WebRequest -Uri https://www.oo-software.com/en/ooregeditor -UseBasicParsing
-		
-	$version_number = ($download_page.ToString() -split "[<>\s]" | sls "\d\d\.\d\.\d\d\d\d" | Select-Object -First 1).ToString()
-	
-	$url32 = $download_page.links | ? href -match 'ooregeditor\.zip' | % href | select -First 1
-	
-	return @{ Version = $version_number; URL32 = $url32; PackageName = 'regeditor' }
+	$download_page = Invoke-WebRequest -Uri https://www.oo-software.com/en/download/current/ooregeditor -UseBasicParsing
+	$version = ($download_page.ToString() -split "[<>\s]" | sls "\d\d\.\d\.\d\d\d\d" | Select-Object -First 1).ToString().trim(",") 
+    $url32 = "https://dl5.oo-software.com/files/ooregeditor12/120/ooregeditor.zip"
+    
+	return @{ Version = $version; URL32 = $url32; PackageName = 'regeditor' }
 }
 
 function global:au_SearchReplace {
@@ -18,7 +17,8 @@ function global:au_SearchReplace {
 }
 
 function global:au_BeforeUpdate() {
-	Get-RemoteFiles -Purge -NoSuffix
+    Throw "Regeditor updated, please find URL manually."
+    #Get-RemoteFiles -Purge -NoSuffix
 }
 
 Update-Package  -ChecksumFor none
