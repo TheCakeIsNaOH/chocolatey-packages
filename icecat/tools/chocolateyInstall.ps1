dc329117d9259cde7f1536eb6cb8592710d4b1dc
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-10-09 16:26:31 -0500

    Icecat initial

---
+++ icecat/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url64          = 'https://github.com/muslayev/icecat-win64/releases/download/v78.3.0/icecat-78.3.0.en-US.win64.installer.exe'
+  checksum64     = 'c0ef5900b2e56971651dadd2558b43804d896cbb9717e16cf475ddbe1963df5f'
+  checksumType64 = 'sha256'
+  silentArgs     = '-ms'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
