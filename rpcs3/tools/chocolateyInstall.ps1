7171ca00c0cfcd0ac15170d7f33572435405368c
Chocolatey <chocolatey@realdimensions.net>
2020-11-12 21:02:37 +0000

    AU: 1 updated - rpcs3 [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/11511a996ef56968630a8d610b094d3846521060

--- rpcs3/tools/chocolateyInstall.ps1
+++ rpcs3/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32                 = 'https://github.com/RPCS3/rpcs3-binaries-win/releases/download/build-829a697c390385e2f07b358deb33bc5bc1e91540/rpcs3-v0.0.13-11222-829a697c_win64.7z'
-$checksum32            = '08be4b13259026742280fdf78d2cab090df0f23237df86437efc97193cad5751'
+$url32                 = 'https://github.com/RPCS3/rpcs3-binaries-win/releases/download/build-1378645413c291ad18e5d18b6d919b366507f9d8/rpcs3-v0.0.13-11232-13786454_win64.7z'
+$checksum32            = 'f972cf1702d0606cee947675023e68066278b25cf4154471757e256529384fc1'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
