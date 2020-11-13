9128c76f8c3773bf3ad87f007958b09cc2b05dfe
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-11-13 16:01:45 -0600

    4k-youtube-to-mp3 back to MSI installer

--- 4k-youtube-to-mp3/tools/chocolateyinstall.ps1
+++ 4k-youtube-to-mp3/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.13.2_online.exe?source=chocolatey'
-$url64                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.13.2_x64_online.exe?source=chocolatey'
-$checksum32            = '1988172c4e84a3ba53521ccc91604f26adc95c94998cc120582f594955d4f51a'
-$checksum64            = '896be65494b7bc2c4749e049855e460fd990d3eca31369fa23bc1e0315f685fb'
+$url32                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.13.3.msi?source=chocolatey'
+$url64                 = 'https://dl.4kdownload.com/app/4kyoutubetomp3_3.13.3_x64.msi?source=chocolatey'
+$checksum32            = '4fcb09745a2d5503b8786c5e6fedc090c042e6f0079c18edd9cbe50af422b6c1'
+$checksum64            = 'af1879827c7f24f5625a104e216502da432105feda560cf139a4fe715d5973d7'
 $pp                    = Get-PackageParameters
 $shortcutName          = '4K YouTube to MP3.lnk'
 $shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'EXE'
+  fileType      = 'MSI'
   url           = $url32
   url64bit      = $url64
   softwareName  = '4K Youtube to MP3*'
@@ -18,7 +18,7 @@ $packageArgs = @{
   checksumType  = 'sha256'
   checksum64    = $checksum64
   checksumType64= 'sha256'
-  silentArgs    = "/quiet /norestart /log `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
--- 4k-youtube-to-mp3/update.ps1
+++ 4k-youtube-to-mp3/update.ps1
@@ -15,12 +15,12 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$download_page = Invoke-WebRequest -Uri https://www.4kdownload.com/download -UseBasicParsing
 	
-	$regex64       = '4kyoutubetomp3_.*_x64_online.exe'
-	$regex32       = '4kyoutubetomp3_[.\d]*_online\.exe'
+	$regex64       = '4kyoutubetomp3_.*_x64.msi'
+	$regex32       = '4kyoutubetomp3_.*\.msi'
     $url64         = $download_page.links | ? href -match $regex64 | select -First 1 -expand href
 	$url32         = $download_page.links | ? href -match $regex32 | select -First 1 -expand href 
 	
-	$version       = $url64 -split '[_]' | select -Last 1 -Skip 2
+	$version       = $url64 -split '[_]' | select -Last 1 -Skip 1
 	
 	$modurl64      = $url64 -replace 'website', 'chocolatey'
 	$modurl32      = $url32 -replace 'website', 'chocolatey'
