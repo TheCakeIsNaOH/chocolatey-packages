4da18a28a07a357f27d82e42cd87fa4a4370294c
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-11-20 13:14:33 -0600

    RPCS3 update to get-toolslocation

--- rpcs3/tools/chocolateyInstall.ps1
+++ rpcs3/tools/chocolateyInstall.ps1
@@ -3,9 +3,8 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 $pp                    = Get-PackageParameters
 $shortcutName          = 'RPCS3.lnk'
 $extractDir            = $(Get-ToolsLocation)
-$extractedDir          = (Join-Path $extractDir 'Dolphin-x64')
-$dolphinDir            = (Join-Path $extractDir 'Dolphin-Dev')
-$exepath               = (Join-Path $dolphinDir 'Dolphin.exe')
+$rpcs3Dir              = (Join-Path $extractDir 'RPCS3')
+$exepath               = (Join-Path $rpcs3Dir 'rpcs3.exe')
 
 $packageArgs = @{
     PackageName  = $env:ChocolateyPackageName
@@ -13,8 +12,22 @@ $packageArgs = @{
     FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.13-11280-5f010c2f_win64.7z'
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
 
-Write-Host "$packageName installed to $toolsDir"
+Write-Host "$packageName installed to $rpcs3Dir"
 
 Remove-Item -Force -Path $toolsDir\*.7z
+
+Install-BinFile -Name 'RPCS3' -Path $exepath -UseStart
+
+if ($pp['desktopicon']) {
+	$desktopicon = (Join-Path ([Environment]::GetFolderPath("CommonDesktop")) $shortcutName)
+	Write-Host -ForegroundColor white 'Adding ' $desktopicon
+	Install-ChocolateyShortcut -ShortcutFilePath $desktopicon -TargetPath $exepath  -RunAsAdmin
+}
+
+if (!$pp['nostart']) {
+	$starticon = (Join-Path ([environment]::GetFolderPath([environment+specialfolder]::CommonPrograms)) $shortcutName)
+	Write-Host -ForegroundColor white 'Adding ' $starticon
+	Install-ChocolateyShortcut -ShortcutFilePath $starticon -TargetPath $exepath  -RunAsAdmin
+}
---
+++ rpcs3/tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir 			   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$extractDir            = $(Get-ToolsLocation)
+$rpcs3Dir              = (Join-Path $extractDir 'RPCS3')
+$exepath               = (Join-Path $rpcs3Dir 'rpcs3.exe')
+$shortcutName          = 'RPCS3.lnk'
+$desktopicon           = (Join-Path ([Environment]::GetFolderPath("CommonDesktop")) $shortcutName)
+$starticon             = (Join-Path ([environment]::GetFolderPath([environment+specialfolder]::CommonPrograms)) $shortcutName)
+
+
+if (Test-Path $desktopicon) {
+	Remove-Item $desktopicon
+	Write-Host -ForegroundColor white 'Removed ' $desktopicon
+} else {
+	Write-Host -ForegroundColor yellow 'Did not find ' $desktopicon 'to remove'
+}
+
+if (Test-Path $starticon) {
+	Remove-Item $starticon
+	Write-Host -ForegroundColor white 'Removed ' $starticon
+} else {
+	Write-Host -ForegroundColor yellow 'Did not find ' $starticon 'to remove'
+}
+
+Uninstall-BinFile -Name 'Dolphin-Beta' -Path $exepath
+
+Write-Host -ForegroundColor white 'Removing ' $rpcs3Dir
+Remove-Item $rpcs3Dir -Recurse -ea 0
