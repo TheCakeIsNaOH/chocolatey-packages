94726424a29442c63e57ca8520f44b79f7f83b7f
Chocolatey <chocolatey@realdimensions.net>
2020-11-20 18:18:04 +0000

    AU: 1 updated - rpcs3 [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/7072208c4c1e42936a2e84d24d8fd09ef0fa7324

--- rpcs3/tools/chocolateyInstall.ps1
+++ rpcs3/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 $packageArgs = @{
     PackageName  = $env:ChocolateyPackageName
     Destination  = $toolsDir
-    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.13-11275-85880ffd_win64.7z'
+    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.13-11280-5f010c2f_win64.7z'
 }
 
 Install-ChocolateyZipPackage @packageArgs
