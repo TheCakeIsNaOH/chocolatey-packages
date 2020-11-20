1c17b97bbb63c3d39f7848abc9a1889ec6346349
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-11-19 20:21:45 -0600

    RPCS3 move to internal

--- rpcs3/tools/chocolateyInstall.ps1
+++ rpcs3/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32                 = 'https://github.com/RPCS3/rpcs3-binaries-win/releases/download/build-36f11934979659468db3ce376a8044763dab833d/rpcs3-v0.0.13-11272-36f11934_win64.7z'
-$checksum32            = '5a57d18005f6b93b97623ddff7574ea31e290205707697ec00fccbd0fbdc05e3'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = $url32
-  checksum      = $checksum32
-  checksumType  = 'sha256'
+    PackageName  = $env:ChocolateyPackageName
+    Destination  = $toolsDir
+    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.13-11274-5b207c3b_win64.7z'
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
+Write-Host "$packageName installed to $toolsDir"
+
+Remove-Item -Force -Path $toolsDir\*.zip
--- rpcs3/update.ps1
+++ rpcs3/update.ps1
@@ -2,13 +2,19 @@ Import-Module AU
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
-			"(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"          
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-		}
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*FileFullPath\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName32)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+            "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        }
 	}
 }
 
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+}
 
 function global:au_GetLatest {
 	$download_page = Invoke-WebRequest -Uri https://rpcs3.net/download -UseBasicParsing
@@ -18,10 +24,9 @@ function global:au_GetLatest {
 	$majorVersion  = ($url32 -split '[-]' | select -Last 1 -Skip 2).trim('v')
     $buildNumber   = $url32 -split '[-]' | select -Last 1 -Skip 1
     
-    
 	$version       = $majorVersion + '-alpha' + $buildNumber
     
 	return @{ Version = $version; URL32 = $url32; }
 }
 
-Update-Package
+Update-Package -ChecksumFor none
