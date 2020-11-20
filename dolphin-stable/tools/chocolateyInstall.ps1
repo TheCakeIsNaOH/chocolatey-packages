9d57252df32e7304d5acf0cd49e0125edc81902d
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-11-20 10:56:23 -0600

    add dolphin stable

---
+++ dolphin-stable/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir 			   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pp                    = Get-PackageParameters
+$shortcutName          = 'Dolphin Emulator (Stable).lnk'
+#FIXME $exepath               = (Join-Path $dolphinDir 'Dolphin.exe')
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  file           = Join-Path $toolsDir 'dolphin-x64-5.0.exe'
+  softwareName   = 'Dolphin*' #FIXME
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -Path $toolsDir\*.exe
+
+#FIXME Install-BinFile -Name 'Dolphin-Dev' -Path $exepath -UseStart
+
+<# if ($pp['desktopicon']) {
+	$desktopicon = (Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName)
+	Write-Host -ForegroundColor white 'Adding ' $desktopicon
+	Install-ChocolateyShortcut -ShortcutFilePath $desktopicon -TargetPath $exepath  -RunAsAdmin
+}
+
+if (!$pp['nostart']) {
+	$starticon = (Join-Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) $shortcutName)
+	Write-Host -ForegroundColor white 'Adding ' $starticon
+	Install-ChocolateyShortcut -ShortcutFilePath $starticon -TargetPath $exepath  -RunAsAdmin
+} #>
---
+++ dolphin-stable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿<# $ErrorActionPreference = 'Stop';
+$toolsDir 			   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$dolphinDir            = (Join-Path $(Get-ToolsLocation) Dolphin-Dev)
+$exepath               = (Join-Path $dolphinDir Dolphin.exe)
+$shortcutName          = 'Dolphin Emulator (Dev).lnk'
+$desktopicon           = (Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName)
+$starticon             = (Join-Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) $shortcutName)
+
+
+if (Test-Path $desktopicon) {
+	Remove-Item $desktopicon
+	Write-Host -ForegroundColor white 'Removed ' $desktopicon
+} else {
+	Write-Host -ForegroundColor yellow 'Did not find ' $desktopicon 'to remove'
+}
+
+if (Test-Path $starticon) {
+	Remove-Item $starticon
+	Write-Host -ForegroundColor white 'Removed ' $starticon
+} else {
+	Write-Host -ForegroundColor yellow 'Did not find ' $starticon 'to remove'
+}
+
+Uninstall-BinFile -Name 'Dolphin-Dev' -Path $exepath
+
+Write-Host -ForegroundColor white 'Removing ' $dolphinDir
+Remove-Item $DolphinDir -Recurse -ea 0 #>
