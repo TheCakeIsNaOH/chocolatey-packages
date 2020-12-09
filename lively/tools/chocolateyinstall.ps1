abe223fe3e35c33c1662a416929cc35b4c58c726
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-12-08 18:50:59 -0600

    add Lively wallpaper

---
+++ lively/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file          = Join-Path $toolsDir ''
+  softwareName  = 'Lively Wallpaper*'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -EA 0 -Path $toolsDir\*.exe
