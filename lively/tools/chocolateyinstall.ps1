4e5c9d68fb9f9683b43b32be55a8575c50166d20
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-12-08 19:13:48 -0600

    Lively add in dependencies and version check

--- lively/tools/chocolateyinstall.ps1
+++ lively/tools/chocolateyinstall.ps1
@@ -1,10 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$IsWin10=[Environment]::OSVersion.Version.Major
+if ($IsWin10 -ne "10") {
+    Write-Warning "This package requires Windows 10."
+	throw
+}
+
+$IsCorrectBuild=[Environment]::OSVersion.Version.Build
+if ($IsCorrectBuild -lt "18362") {
+    Write-Warning "This package requires at least Windows 10 version 1903"
+	throw
+}
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  file          = Join-Path $toolsDir ''
+  file          = "$toolsDir\lively_setup_x86_full_v1160.exe"
   softwareName  = 'Lively Wallpaper*'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
