b21e0d0463b1a2988e42ce40702a08e3779cbd47
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-01-07 18:17:17 -0600

    Add procrastitracker initial

---
+++ procrastitracker/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop'
+
+Remove-Process -NameFilter "procrastitracker"
---
+++ procrastitracker/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Remove-Process -NameFilter "procrastitracker"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file          = Join-Path $toolsDir 'ProcrastiTracker_Setup.exe'
+  softwareName  = 'ProcrastiTracker'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -EA 0 -Path $toolsDir\*.exe
