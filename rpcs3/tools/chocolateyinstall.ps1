f8a782b03377984a164af94b80bf88df5024c54a
Chocolatey <chocolatey@realdimensions.net>
2021-03-14 22:02:16 +0000

    AU: 1 updated - rpcs3 [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1

--- rpcs3/tools/chocolateyinstall.ps1
+++ rpcs3/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $exepath               = (Join-Path $rpcs3Dir 'rpcs3.exe')
 $packageArgs = @{
     PackageName  = $env:ChocolateyPackageName
     Destination  = $rpcs3Dir
-    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.15-11950-c9214ce6_win64.7z'
+    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.15-11958-6a148493_win64.7z'
 }
 
 Write-Host "Moving existing files from $toolsDir to rpcs3Dir"
