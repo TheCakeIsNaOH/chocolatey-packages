80845cd8a0c0ffa6a94c9a3ba19eb455d6dbe5bd
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-03-22 11:30:27 -0500

    hidevolumeosd initial

---
+++ hidevolumeosd/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  file           = Join-Path $toolsDir 'HideVolumeOSD-1.3.exe'
+  softwareName   = 'HideVolumeOSD*'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item "$toolsDir\*.exe" -Force -EA SilentlyContinue | Out-Null
