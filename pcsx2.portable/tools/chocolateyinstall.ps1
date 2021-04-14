535940f5a59fff31141881946040208c2b1fe8d3
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-04-14 13:09:13 -0500

    PCSX2.portable explicit join-path, install location, misc

--- pcsx2.portable/tools/chocolateyinstall.ps1
+++ pcsx2.portable/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation          = (Get-ChildItem -Path $toolsDir -Filter "pcsx2*.7z").fullname
 $pp                    = Get-PackageParameters
 $shortcutName          = 'PCSX2 Portable.lnk'
 $startPath             = Join-Path ([Environment]::GetFolderPath("CommonStartMenu")) $shortcutName
 $desktopPath           = Join-Path ([Environment]::GetFolderPath("CommonDesktop")) $shortcutName
 $tempPath              = Join-Path $env:temp 'PCSX2-portable'
 
+Remove-Item -Recurse -ea 0 -Path $tempPath 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  FileFullPath  = $fileLocation
+  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.6.0-binaries.7z'
   Destination   = "$tempPath"
 }
 
@@ -25,7 +25,7 @@ if ($pp['Path']) {
 	$destination = Join-Path $(Get-ToolsLocation) 'PCSX2'
 }
 
-Mkdir -Path $destination -Force | Out-Null
+$null = New-Item -ItemType Directory -Path $destination -Force
 
 $configFile = [IO.Path]::Combine("$destination" , "Shaders" , "GSdx_FX_Settings.ini")
 if (Test-Path $configFile) {
@@ -33,19 +33,25 @@ if (Test-Path $configFile) {
 	Rename-Item -Path $configFile -Force -ea 0 -NewName ("GSdx_FX_Settings-" + (Get-Date -Format "yyyy-MM-dd").tostring() + "-backup.ini")
 }
 
+Write-Host -ForegroundColor white "Copying files to $destination"
+
 $innerFolder = (Get-ChildItem -Path "$tempPath" -Filter "PCSX2*").FullName
 $fileList = Get-ChildItem -Path $innerFolder | Copy-Item -Destination $destination -Recurse -Force -PassThru
 $fileList | select -ExpandProperty FullName | Out-File -Force -FilePath (Join-Path $toolsDir 'install-files.txt')
 
 $exePath = Join-Path "$destination" "pcsx2.exe"
 
-if ($pp['Start']) {
+if (!$pp['nostart']) {
+    Write-Host -ForegroundColor white 'Adding ' $startPath
 	Install-ChocolateyShortcut -ShortcutFilePath "$startPath" -TargetPath "$exePath" -WorkingDirectory "$destination"
 }
 
 if ($pp['Desktop']) {
+    Write-Host -ForegroundColor white 'Adding ' $desktopPath
 	Install-ChocolateyShortcut -ShortcutFilePath "$desktopPath" -TargetPath "$exePath" -WorkingDirectory "$destination"
 }
 
-Remove-Item -ea 0 -Path $fileLocation
+Remove-Item -ea 0 -Path $toolsDir\*.7z
 Remove-Item -Recurse -ea 0 -Path $tempPath 
+
+$env:ChocolateyPackageInstallLocation = $destination
