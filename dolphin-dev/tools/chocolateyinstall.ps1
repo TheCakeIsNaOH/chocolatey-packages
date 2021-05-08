761e23c939c1193821d6063845df0c4f1468ca4d
Chocolatey <chocolatey@realdimensions.net>
2021-05-08 14:01:49 +0000

    AU: 1 updated - dolphin-dev [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1

--- dolphin-dev/tools/chocolateyinstall.ps1
+++ dolphin-dev/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $dolphinDir            = (Join-Path $extractDir 'Dolphin-Dev')
 $exepath               = (Join-Path $dolphinDir 'Dolphin.exe')
 
 $packageArgs = @{
-  FileFullPath64 = Join-Path $toolsDir 'dolphin-master-5.0-14107-x64.7z'
+  FileFullPath64 = Join-Path $toolsDir 'dolphin-master-5.0-14109-x64.7z'
   Destination    = $extractDir
   PackageName    = $env:ChocolateyPackageName
 }
