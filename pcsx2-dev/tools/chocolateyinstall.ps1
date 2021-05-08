c0296a90cee161dcb1f22d1f095ab819d5733db0
Chocolatey <chocolatey@realdimensions.net>
2021-05-08 18:02:14 +0000

    AU: 1 updated - pcsx2-dev [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1

--- pcsx2-dev/tools/chocolateyinstall.ps1
+++ pcsx2-dev/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ Remove-Item -Recurse -ea 0 -Path $tempPath
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1233-g2e0baa716.7z'
+  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1235-g6a2ed3d07.7z'
   Destination   = "$tempPath"
 }
 
