e27c60d110aac852898d8bd14fdef27d03aac6ac
Chocolatey <chocolatey@realdimensions.net>
2021-05-31 18:02:44 +0000

    AU: 2 updated - dolphin-dev rpcs3 [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1

--- rpcs3/tools/chocolateyinstall.ps1
+++ rpcs3/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $exepath               = (Join-Path $rpcs3Dir 'rpcs3.exe')
 $packageArgs = @{
     PackageName  = $env:ChocolateyPackageName
     Destination  = $rpcs3Dir
-    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.16-12346-4a32b70a_win64.7z'
+    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.16-12349-a1608b63_win64.7z'
 }
 
 Write-Host "Moving existing files from $toolsDir to rpcs3Dir"
