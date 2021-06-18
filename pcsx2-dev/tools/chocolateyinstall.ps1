d2e8fbe91b89203021f5102c16b8b34b65335b57
Chocolatey <chocolatey@realdimensions.net>
2021-06-18 18:02:24 +0000

    AU: 3 updated - balabolka pcsx2-dev rpcs3 [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1

--- pcsx2-dev/tools/chocolateyinstall.ps1
+++ pcsx2-dev/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ Remove-Item -Recurse -ea 0 -Path $tempPath
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1288-gb5a917fb0.7z'
+  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1290-g33d8290fd.7z'
   Destination   = "$tempPath"
 }
 
