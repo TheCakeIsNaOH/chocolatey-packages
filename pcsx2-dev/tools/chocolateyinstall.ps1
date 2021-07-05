cbf892ba73309b0699810479327c844ee0ce83e3
Chocolatey <chocolatey@realdimensions.net>
2021-07-05 23:21:08 +0000

    AU: 3 updated - dolphin-dev pcsx2-dev vikunja-desktop [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/2d4efd960aeab76ce41113c0eb8b8db6c8a026cc

--- pcsx2-dev/tools/chocolateyinstall.ps1
+++ pcsx2-dev/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ Remove-Item -Recurse -ea 0 -Path $tempPath
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1430-g627f216bf.7z'
+  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1433-g6dd90aeae.7z'
   Destination   = "$tempPath"
 }
 
