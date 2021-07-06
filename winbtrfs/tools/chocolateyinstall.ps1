11e68bfc687df3bc6e9bfc1209143ba3cb11ae86
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-07-05 22:01:20 -0500

    winbtrfs switch to join-path and seperate legal folder

--- winbtrfs/tools/chocolateyinstall.ps1
+++ winbtrfs/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation          = (Get-ChildItem $toolsDir -Filter "*.zip").FullName
+$fileLocation          = Join-Path $toolsDir 'btrfs-1.7.8.1.zip'
 $driverFile            = Join-Path $toolsDir 'btrfs.cat'
 $outputFile            = Join-Path $toolsDir 'MarkHarmstone.cer'
 $exportType            = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
