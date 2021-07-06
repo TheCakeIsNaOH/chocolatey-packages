3499c3861d2fe243bece6a972b18e127ae1a99c3
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-07-05 21:48:53 -0500

    innoextract move to join-path and seperate legal folder

--- innoextract/tools/chocolateyinstall.ps1
+++ innoextract/tools/chocolateyinstall.ps1
@@ -1,7 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation          = (Get-ChildItem $toolsDir -Filter "*.zip").FullName
 
-Get-ChocolateyUnzip $fileLocation $toolsDir
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  FileFullPath  = Join-Path $toolsDir 'innoextract-1.9-windows.zip'
+  Destination   = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs
 
 Remove-Item -Force -EA 0 -Path $toolsDir\*.zip
