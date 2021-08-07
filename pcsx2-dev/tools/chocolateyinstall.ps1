60c9c348dff75e085b4a679695bb66029844c813
Chocolatey <chocolatey@realdimensions.net>
2021-08-07 18:21:53 +0000

    AU: 3 updated - dolphin-dev pcsx2-dev rpcs3 [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/fff8b023295bbe7f3931f0765b6b3de8ce8b4eac

--- pcsx2-dev/tools/chocolateyinstall.ps1
+++ pcsx2-dev/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ Remove-Item -Recurse -ea 0 -Path $tempPath
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1578-ga982852e3.7z'
+  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1589-g0c5df806b.7z'
   Destination   = "$tempPath"
 }
 
