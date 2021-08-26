a5e4bde68750a902f24d8e46c7f5c47bbfebb9e8
Chocolatey <chocolatey@realdimensions.net>
2021-08-26 18:06:33 +0000

    AU: 5 updated - dolphin-dev nextcloud-client pcsx2-dev rpcs3 vikunja-desktop-unstable [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/a2d101bdb2589882974e17a3aa96cb35571286c7

--- pcsx2-dev/tools/chocolateyinstall.ps1
+++ pcsx2-dev/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ Remove-Item -Recurse -ea 0 -Path $tempPath
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1641-gbafb0db6d.7z'
+  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1644-gfeaf5ba5f.7z'
   Destination   = "$tempPath"
 }
 
