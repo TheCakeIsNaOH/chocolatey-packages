3949264c95e411e2c77da0f2301002cd52af4d73
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-08-30 13:46:47 -0500

    4k tokkit initial

---
+++ 4k-tokkit/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32                 = 'https://dl.4kdownload.com/app/4kvideodownloader_4.17.1.msi?source=chocolatey'
+$url64                 = 'https://dl.4kdownload.com/app/4kvideodownloader_4.17.1_x64.msi?source=chocolatey'
+$pp                    = Get-PackageParameters
+$shortcutName          = '4K Tokkit.lnk'
+$shortcut              = Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName
+$checksum32            = '9a1d44902b52c2654d4b0e72331bdd038683adbcc31beea2a4b307fcd9fd22dd'
+$checksum64            = '0c0d6c827dad81527eed5eaf3174cefa93457fb994544d709d7d3e871defc8cc'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  url           = $url32
+  url64bit      = $url64
+  softwareName  = '4K Tokkit*'
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+if ($pp['noicon']) {
+	if (Test-Path $shortcut) {
+		Remove-Item $shortcut
+		Write-Host -ForegroundColor green 'Removed ' $shortcut
+	} else {
+		Write-Host -ForegroundColor yellow 'Did not find ' $shortcut
+	}
+}
