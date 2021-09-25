357cbcbd882d79b50d19999331faef5dcae7a18f
Chocolatey <chocolatey@realdimensions.net>
2021-09-25 18:15:27 +0000

    AU: 4 updated - pcsx2-dev rpcs3 vikunja-desktop-unstable win-acme [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/86538f78eac866ee81e8ef950fa617b99ca5baa6

--- rpcs3/tools/chocolateyinstall.ps1
+++ rpcs3/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $exepath               = (Join-Path $rpcs3Dir 'rpcs3.exe')
 $packageArgs = @{
     PackageName  = $env:ChocolateyPackageName
     Destination  = $rpcs3Dir
-    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.18-12816-7167fbf5_win64.7z'
+    FileFullPath = Join-Path $toolsDir 'rpcs3-v0.0.18-12821-269c4604_win64.7z'
 }
 
 Write-Host "Moving existing files from $toolsDir to rpcs3Dir"
