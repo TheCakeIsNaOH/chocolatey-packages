75fd9e3c244e508fd3678ab590cf7d5814792549
Chocolatey <chocolatey@realdimensions.net>
2021-10-03 18:21:59 +0000

    AU: 1 updated - win-acme [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/8cd1c16efdc2e0ae9787ecf64e66f3f6827ce200

--- win-acme/tools/chocolateyinstall.ps1
+++ win-acme/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 $packageArgs = @{
     packageName    = "$env:chocolateyPackageName"
     destination    = $toolsDir
-    FileFullPath   = Join-Path $toolsDir 'win-acme.v2.1.19.1138.x86.pluggable.zip'
-    FileFullPath64 = Join-Path $toolsDir 'win-acme.v2.1.19.1138.x64.pluggable.zip'
+    FileFullPath   = Join-Path $toolsDir 'win-acme.v2.1.19.1142.x86.pluggable.zip'
+    FileFullPath64 = Join-Path $toolsDir 'win-acme.v2.1.19.1142.x64.pluggable.zip'
 }
 
 Get-ChocolateyUnzip @packageArgs
