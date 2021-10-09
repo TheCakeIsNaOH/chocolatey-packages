2b0b02ba4b109271ed78d19ee1b2e6d50ea171cc
Chocolatey <chocolatey@realdimensions.net>
2021-10-09 18:13:21 +0000

    AU: 4 updated - dolphin-dev pcsx2-dev rpcs3 vikunja-desktop-unstable [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/aa0bc715d97e683f28e5fda9ce3a1877db0c8204

--- pcsx2-dev/tools/chocolateyinstall.ps1
+++ pcsx2-dev/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ Remove-Item -Recurse -ea 0 -Path $tempPath
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1911-ga2ba0ae818.7z'
+  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1915-g6ed00107cf.7z'
   Destination   = "$tempPath"
 }
 
