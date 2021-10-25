278fcc6bea9d4fe1b55b4fe7a523297fbf1e0843
Chocolatey <chocolatey@realdimensions.net>
2021-10-25 18:15:08 +0000

    AU: 3 updated - balabolka dolphin-dev pcsx2-dev [skip ci] https://gist.github.com/ce0572a342ee6b22e44239db8c54f8b1/e4eda9375f8b6cdc57a26bbcfc9719b5ceb90896

--- pcsx2-dev/tools/chocolateyinstall.ps1
+++ pcsx2-dev/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ Remove-Item -Recurse -ea 0 -Path $tempPath
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1957-gb63f6e807c.7z'
+  FileFullPath  = Join-Path $toolsDir 'pcsx2-1.7.0-dev-1959-gbb494af6b0.7z'
   Destination   = "$tempPath"
 }
 
