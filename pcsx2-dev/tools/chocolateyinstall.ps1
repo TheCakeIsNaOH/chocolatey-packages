88dc5a51e309ddc4713fee404fec5ac9b81fd876
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2022-12-26 09:51:14 -0600

    (pcsx2-dev) remove wxwidgets

--- pcsx2-dev/tools/chocolateyinstall.ps1
+++ pcsx2-dev/tools/chocolateyinstall.ps1
@@ -12,10 +12,8 @@ if ((Get-OSArchitectureWidth -compare 32) -or ($env:chocolateyForceX86 -eq $true
     Throw "32-bit builds have been dropped. Install version 1.7.2484-dev or older for a 32 bit build"
 }
 
-$SSE4_Qt = Join-Path $toolsDir 'pcsx2-v1.7.3764-windows-64bit-SSE4-Qt.7z'
-$SSE4_wxWidgets = Join-Path $toolsDir 'pcsx2-v1.7.3764-windows-64bit-SSE4-wxWidgets.7z'
-$AVX2_Qt = Join-Path $toolsDir 'pcsx2-v1.7.3764-windows-64bit-AVX2-Qt.7z'
-$AVX2_wxWidgets = Join-Path $toolsDir 'pcsx2-v1.7.3764-windows-64bit-AVX2-wxWidgets.7z'
+$SSE4_Qt = Join-Path $toolsDir 'pcsx2-v1.7.3753-windows-64bit-SSE4-Qt.7z'
+$AVX2_Qt = Join-Path $toolsDir 'pcsx2-v1.7.3753-windows-64bit-AVX2-Qt.7z'
 
 
 if ($pp['Path']) {
@@ -27,22 +25,14 @@ if ($pp['Path']) {
 	$destination = Join-Path $(Get-ToolsLocation) 'PCSX2-Dev'
 }
 
-if ($pp['UseAvx2'] -and $pp['UseQt']) {
+if ($pp['UseQt']) {
     Write-Output "Installing AVX2 QT build. See package description for how to switch which build is installed"
     $file64 = $AVX2_Qt
     $exePath = Join-Path "$destination" "pcsx2-qtx64-avx2.exe"
-} elseif ($pp['UseAvx2']) {
-    Write-Output "Installing AVX wxWidgets build. See package description for how to switch which build is installed"
-    $file64 = $AVX2_wxWidgets
-    $exePath = Join-Path "$destination" "pcsx2x64-avx2.exe"
-} elseif ($pp['UseQT']) {
+} else {
     Write-Output "Installing SSE4 QT build. See package description for how to switch which build is installed"
     $file64 = $SSE4_Qt
     $exePath = Join-Path "$destination" "pcsx2-qtx64.exe"
-} else {
-    Write-Output "Installing SSE4 wxWidgets build. See package description for how to switch which build is installed"
-    $file64 = $SSE4_wxWidgets
-    $exePath = Join-Path "$destination" "pcsx2x64.exe"
 }
 
 $packageArgs = @{
--- pcsx2-dev/update.ps1
+++ pcsx2-dev/update.ps1
@@ -6,19 +6,13 @@ function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1" = @{
             '(?i)(^\s*\$avx2_Qt\s*=\s*)(.*)' = "`$1Join-Path `$toolsDir '$($Latest.filename_avx2_qt)'"
-            '(?i)(^\s*\$avx2_wxWidgets\s*=\s*)(.*)' = "`$1Join-Path `$toolsDir '$($Latest.filename_avx2_wxwidgets)'"
             '(?i)(^\s*\$SSE4_Qt\s*=\s*)(.*)' = "`$1Join-Path `$toolsDir '$($Latest.filename_sse4_qt)'"
-            '(?i)(^\s*\$SSE4_wxWidgets\s*=\s*)(.*)' = "`$1Join-Path `$toolsDir '$($Latest.filename_sse4_wxwidgets)'"
         }
         ".\legal\VERIFICATION.txt" = @{
             "(?i)(\s+AVX2_Qt:).*"            = "`${1} $($Latest.url_avx2_qt)"
-            "(?i)(\s+AVX2_wxWidgets:).*"     = "`${1} $($Latest.url_avx2_wxwidgets)"
             "(?i)(\s+SSE4_Qt:).*"            = "`${1} $($Latest.url_sse4_qt)"
-            "(?i)(\s+SSE4_wxWidgets:).*"     = "`${1} $($Latest.url_sse4_wxwidgets)"
             "(?i)(checksum_AVX2_Qt:).*"         = "`${1} $($Latest.checksum_avx2_qt)"
-            "(?i)(checksum_AVX2_wxWidgets:).*"  = "`${1} $($Latest.checksum_avx2_wxWidgets)"
             "(?i)(checksum_SSE4_Qt:).*"         = "`${1} $($Latest.checksum_SSE4_qt)"
-            "(?i)(checksum_SSE4_wxWidgets:).*"  = "`${1} $($Latest.checksum_SSE4_wxWidgets)"
         }
 	}
 }
@@ -28,22 +22,11 @@ function global:au_BeforeUpdate {
     Get-RemoteFiles -Purge -NoSuffix
     $Latest.filename_avx2_qt = $Latest.Filename64
     $Latest.checksum_avx2_qt = $Latest.Checksum64
-    
-    $Latest.URL64 = $Latest.url_avx2_wxwidgets
-    Get-RemoteFiles -NoSuffix
-    $Latest.filename_avx2_wxwidgets = $Latest.Filename64
-    $Latest.checksum_avx2_wxwidgets = $Latest.Checksum64
-    
+   
     $Latest.URL64 = $Latest.url_sse4_qt 
     Get-RemoteFiles -NoSuffix
     $Latest.filename_sse4_qt = $Latest.Filename64
     $Latest.checksum_sse4_qt = $Latest.Checksum64
-    
-    $Latest.URL64 = $Latest.url_sse4_wxwidgets
-    Get-RemoteFiles -NoSuffix
-    $Latest.filename_sse4_wxwidgets = $Latest.Filename64
-    $Latest.checksum_sse4_wxwidgets = $Latest.Checksum64
-    
 }
 
 function global:au_GetLatest {
@@ -51,25 +34,16 @@ function global:au_GetLatest {
     
     $regex_avx2_qt = "pcsx2-v[\d\.]*-windows-64bit-AVX2-Qt.7z"
     $url_avx2_qt   = 'https://github.com' + ($download_page.links | ? href -match $regex_avx2_qt | select -First 1 -expand href)
-    
-    $regex_avx2_wxWidgets  = "pcsx2-v[\d\.]*-windows-64bit-AVX2-wxWidgets.7z"
-    $url_avx2_wxWidgets   = 'https://github.com' + ($download_page.links | ? href -match $regex_avx2_wxWidgets | select -First 1 -expand href)
-   
+
     $regex_sse4_qt = "pcsx2-v[\d\.]*-windows-64bit-sse4-Qt.7z"
     $url_sse4_qt   = 'https://github.com' + ($download_page.links | ? href -match $regex_sse4_qt | select -First 1 -expand href)
-    
-    $regex_sse4_wxWidgets  = "pcsx2-v[\d\.]*-windows-64bit-sse4-wxWidgets.7z"
-    $url_sse4_wxWidgets   = 'https://github.com' + ($download_page.links | ? href -match $regex_sse4_wxWidgets | select -First 1 -expand href)
-	
+
 	$version   = ($url_avx2_qt -split "/" | select -Last 1 -Skip 1).trim("v") + '-dev'
-	
 	return @{ 
         Version = $version
         FileType = "7z"
         url_avx2_qt = $url_avx2_qt
-        url_avx2_wxWidgets = $url_avx2_wxWidgets
         url_sse4_qt = $url_sse4_qt
-        url_sse4_wxWidgets = $url_sse4_wxWidgets
     }
 }
 
