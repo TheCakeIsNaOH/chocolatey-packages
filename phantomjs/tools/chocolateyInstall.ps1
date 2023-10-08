960afae847cfa3003811e9638f119fabb22fc6c0
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2023-10-08 16:44:03 -0500

    Add phantomjs

---
+++ phantomjs/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  FileFullPath   = Join-Path $toolsDir 'phantomjs-2.1.1-windows.zip'
+  Destination    = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$null = Remove-Item "$toolsDir\*.zip" -Force -EA SilentlyContinue
