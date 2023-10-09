fa873fa699528fa59821a85c2c0296155d22da5a
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2023-10-08 23:27:30 -0500

    Add openzfsonwindows

---
+++ openzfsonwindows/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file64        = Join-Path $toolsDir 'OpenZFSOnWindows-debug-2.2.0rc5'
+  softwareName  = 'OpenZFS On Windows'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0) 
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item $fileLocation -force -ea 0 
