6624b1a5aba0d684f870ff0f1d0ef918423aa956
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2023-11-04 22:17:32 -0500

    fix partition assistant updater

--- partition-assistant-standard/update.ps1
+++ partition-assistant-standard/update.ps1
@@ -14,16 +14,23 @@ function global:au_BeforeUpdate() {
 }
 
 function global:au_GetLatest {
-  $releaseNotesUrl = 'https://www.diskpart.com/changelog.html'
-  $downloadPage    = 'https://www.aomeitech.com/pa/standard.html'
+  #$releaseNotesUrl = 'https://www.diskpart.com/changelog.html'
+  #$downloadPage    = 'https://www.aomeitech.com/pa/standard.html'
   
-  $release_page    = Invoke-WebRequest -Uri $releaseNotesUrl -UseBasicParsing
-  $version         = $release_page.links | % {
-      $_.outerHTML -split "<|>" | Where-Object { $_ -match '^\d\d\.' } | select-object -First 1
-  } | select-object -First 1
+  #$release_page    = Invoke-WebRequest -Uri $releaseNotesUrl -UseBasicParsing
+  #$version         = $release_page.links | % {
+  #    $_.outerHTML -split "<|>" | Where-Object { $_ -match '^\d\d\.' } | select-object -First 1
+  #} | select-object -First 1
   
   #$download_page    = Invoke-WebRequest -Uri $downloadPage -UseBasicParsing
   #$url32            = $download_page.links | Where-Object href -match ".exe" | Select-Object -First 1
+  
+  $upgradeINIUrl = "http://www2.aomeisoftware.com/download/Autoupgrade/PAaotoupgrade/PAupgrade.ini"
+  $upgradeINI = ((New-Object System.Net.WebClient).DownloadString($upgradeINIUrl))
+  $majorVersion = ($upgradeINI -split "\n" | Select-String "maj" | select -First 1).tostring().split("=") | Select-Object -Last 1
+  $minorVersion = ($upgradeINI -split "\n" | Select-String "min" | select -First 1).tostring().split("=") | Select-Object -Last 1
+  $patchVersion = ($upgradeINI -split "\n" | Select-String "thrid" | select -First 1).tostring().split("=") | Select-Object -Last 1
+  $version = $majorVersion + "." + $minorVersion + "." + $patchVersion
   
   $url32 = 'https://www.aomeitech.com/ss/download/pa/PAssist_Std.exe'
 
