b7d88c5b10edd3857454a4d86d66de2bf0f22d83
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-01-01 14:08:02 -0600

    add cloudflare win-acme plugin

---
+++ win-acme-validation-dns-cloudflare/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    FilePath   = 'win-acme.v2.2.6.1571.x86.pluggable.zip'
+}
+
+Uninstall-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -ZipFileName $packageArgs.FilePath
---
+++ win-acme-validation-dns-cloudflare/update.ps1
@@ -0,0 +1,38 @@
+Import-Module AU
+. $([System.IO.Path]::Combine("..", '_scripts', 'Get-GitHubLatestReleaseLinks.ps1'))
+
+function global:au_SearchReplace {
+    @{
+        'win-acme-validation-dns-cloudflare.nuspec' = @{
+            "(^.+version=`")(.*)(`".+$)"   = "`$1$($Latest.Version)`$3"
+        }
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*FileFullPath\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName32)'"
+        }
+        ".\tools\chocolateyuninstall.ps1" = @{
+            "(?i)(^\s*FilePath\s*=\s*)(.*)" = "`$1'$($Latest.FileName32)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+            "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        }
+	}
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_GetLatest {
+    $download_page = Get-GitHubLatestReleaseLinks -User "win-acme" -Repository "win-acme"
+	
+    $match32 = "plugin.validation.dns.cloudflare.*\.zip"
+    
+    $url32      = 'https://github.com' + ($download_page.links | ? href -match $match32 | Select-Object -First 1 -ExpandProperty href)
+    
+	$version    = ($url32 -split '/' | select -Last 1 -Skip 1).trim("v")
+    
+	return @{ Version = $version; URL32 = $url32 }
+}
+
+Update-Package -ChecksumFor none
