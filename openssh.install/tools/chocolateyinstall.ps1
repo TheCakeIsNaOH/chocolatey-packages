425c900cca7c07cb80d144c686168239679be208
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-01-10 21:41:08 -0600

    Openssh initial

---
+++ openssh.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  file          = Join-Path $toolsDir '.msi'
+  file64        = Join-Path $toolsDir '.msi'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+  softwareName  = 'OpenSSH*'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -EA 0 -Path $toolsDir\*.msi
