a8a758f458373c90a0e37bdc076a759710059368
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-01-14 23:57:53 -0600

    Add win-acme-validation-dns-dreamhost

---
+++ win-acme-validation-dns-dreamhost/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    FilePath   = 'plugin.validation.dns.dreamhost.v2.2.6.1571.zip'
+}
+
+Uninstall-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -ZipFileName $packageArgs.FilePath
