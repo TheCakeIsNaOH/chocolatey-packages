48a0e6b32d3421027a1b5f165c79a4a96870ea9d
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-01-20 23:24:08 -0600

    Add googlechromedev

---
+++ googlechromedev/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\helpers.ps1
+
+$version = '120.0.6099.56-dev'
+if ($version -le (Get-ChromeDevVersion)) {
+  Write-Host "Google Chrome Dev $version is already installed."
+  return
+}
+
+$packageArgs = @{
+  packageName            = 'googlechrome'
+  fileType               = 'MSI'
+  url                    = 'https://dl.google.com/tag/s/dl/chrome/'
+  url64bit               = 'https://dl.google.com/tag/s/dl/chrome/'
+  checksum               = '1234'
+  checksum64             = '1234'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes         = @(0)
+}
+
+if (Get-Chrome32bitInstalled) { 'url64bit', 'checksum64', 'checksumType64' | ForEach-Object { $packageArgs.Remove($_) } }
+Install-ChocolateyPackage @packageArgs
---
+++ googlechromedev/update.ps1
@@ -0,0 +1,35 @@
+﻿import-module au
+
+$versionsApi = 'https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/dev/versions'
+
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+  $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^[$]version\s*=\s*)('.*')" = "`$1'$($Latest.RemoteVersion)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $versions_info = Invoke-RestMethod -Method Get -UseBasicParsing -Uri $versionsApi
+  $version = $versions_info.versions | Select-Object -First 1 -ExpandProperty version
+  $version = "$version-dev"
+
+  @{
+    URL32 = 'https://dl.google.com/tag/s/dl/chrome/install/dev/googlechromedevstandaloneenterprise.msi'
+    URL64 = 'https://dl.google.com/tag/s/dl/chrome/install/dev/googlechromedevstandaloneenterprise64.msi'
+    Version = $version
+    RemoteVersion = $version
+  }
+}
+
+update -ChecksumFor none
