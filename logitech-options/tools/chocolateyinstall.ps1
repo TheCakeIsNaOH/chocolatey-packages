3cacf17530bced8a3d065306b658f6d8b07b4128
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-02-08 12:05:54 -0600

    Logitech options refactor

--- logitech-options/tools/chocolateyinstall.ps1
+++ logitech-options/tools/chocolateyinstall.ps1
@@ -1,14 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe'
-$checksum = '062bb8652108eaa6c77dc5528c18b33d4b9ad73c0687f51d54dafee7223a2019'
 
 Confirm-Win10
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = $url
-    checkum        = $checksum
+    url            = 'https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe'
+    checkum        = '062bb8652108eaa6c77dc5528c18b33d4b9ad73c0687f51d54dafee7223a2019'
     checksumType   = 'sha256'
     fileType       = 'EXE'
     silentArgs     = '/quiet /update:false'
