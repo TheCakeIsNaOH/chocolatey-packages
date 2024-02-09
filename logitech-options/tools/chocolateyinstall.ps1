f36d5ab0824300678c25ff7021c804788edc35fa
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-02-08 21:41:16 -0600

    Fix logitech-options again

--- logitech-options/tools/chocolateyinstall.ps1
+++ logitech-options/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ Confirm-Win10
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = 'https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe'
-    checkum        = '062bb8652108eaa6c77dc5528c18b33d4b9ad73c0687f51d54dafee7223a2019'
+    checksum        = '062bb8652108eaa6c77dc5528c18b33d4b9ad73c0687f51d54dafee7223a2019'
     checksumType   = 'sha256'
     fileType       = 'EXE'
     silentArgs     = '/quiet /update:false'
