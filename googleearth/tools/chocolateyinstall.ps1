35e361d3a32d293f9a38e110938858d8f74e576d
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-02-10 22:21:07 -0600

    Add googleearth package

---
+++ googleearth/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE' 
+  url           = 'https://dl.google.com/earth/client/GE7/release_7_1_8/googleearth-win-7.1.8.3036.exe' 
+  softwareName  = 'Google Earth*' 
+  checksum      = 'B64FFC6F31E3E42F154E7D19DFA301376DE987403A9C50B575DAC04022E06576'
+  checksumType  = 'sha256'
+  silentArgs    = 'OMAHA=1'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs 
