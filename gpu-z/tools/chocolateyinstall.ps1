839691459228cf0e17c68291faaf1ed48995d0ec
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-02-28 09:11:52 -0600

    Add gpu-z

---
+++ gpu-z/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$file        = 'GPU-Z.2.57.0.exe'
+
+Remove-Item $ENV:ChocolateyInstall\bin\gpu-z*.exe  # delete old shims
+Remove-Item $toolsDir\*.ignore                     # delete old .ignore files (2.18.0+)
+Remove-Item $toolsDir\*.exe -Exclude $file         # delete old versions kept by upgrade
+Write-Host "" | Out-File "$toolsDir\$file.ignore"  # create new .ignore file so shim isn't created
+
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\GPU-Z.lnk" -targetPath "$toolsDir\$file"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\GPU-Z.lnk" -targetPath "$toolsDir\$file"
---
+++ gpu-z/update.ps1
@@ -0,0 +1,34 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        }
+        ".\tools\chocolateyinstall.ps1" = @{
+            '(?i)(^\$File\s*=\s*)(.*)' = "`$1'$($Latest.FileName32)'"
+        }
+	}
+}
+
+function global:au_BeforeUpdate() {
+    Get-RemoteFiles -Purge -NoSuffix 
+}
+
+
+function global:au_GetLatest {
+    $versionData = Invoke-RestMethod -Uri "https://www.gpu-z.com/gpuz/version_v2" -UseBasicParsing
+    $version     = $versionData.latest
+    $url         = 'https://us2-dl.techpowerup.com/files/GPU-Z.' + $version + '.exe'
+    $userAgent   = 'Scoop/1.0'
+
+    return @{ 
+        Version = $version; 
+        URL32 = $url; 
+        PackageName = 'gpu-z'
+        Options  = @{ Headers = @{ 'User-Agent' = $useragent } }
+    }
+}
+
+Update-Package  -ChecksumFor none
