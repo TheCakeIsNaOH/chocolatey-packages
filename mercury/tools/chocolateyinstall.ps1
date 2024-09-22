fe378abd165c44896eb8c3f6494ebd0934dde4dd
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-09-21 22:10:58 -0500

    Add mercury package

---
+++ mercury/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  file64         = Join-Path $toolsDir '.exe'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Mercury*'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
---
+++ mercury/update.ps1
@@ -0,0 +1,32 @@
+﻿Import-Module Chocolatey-AU
+. $([System.IO.Path]::Combine("..", '_scripts', 'Get-GitHubLatestReleaseLinks.ps1'))
+
+function global:au_GetLatest {
+    $download_page = Get-GitHubLatestReleaseLinks -User "Alex313031" -Repository "Mercury"
+	
+    $url64 = $download_page.links | ? { $_.href -match 'AVX2_installer.exe$' } | select -expand href -First 1
+    $version = ($url64 -split '/' | select -Last 1 -Skip 1).trim("v.")
+
+    @{
+        URL64      = "https://github.com" + $url64
+        Version    = $version
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+            "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+        }
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*File64\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName64)'"
+        }   
+    }
+}
+
+function global:au_BeforeUpdate {
+     Get-RemoteFiles -Purge
+}
+
+Update-Package -ChecksumFor none
