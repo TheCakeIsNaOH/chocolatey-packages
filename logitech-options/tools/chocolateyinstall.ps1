c83345ae3f0d36acfc444fcf69f026762dfc4caf
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-11-07 20:52:21 -0600

    Update logitech-options

--- logitech-options/tools/chocolateyinstall.ps1
+++ logitech-options/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ Confirm-Win10
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = 'https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe'
-    checksum        = '062bb8652108eaa6c77dc5528c18b33d4b9ad73c0687f51d54dafee7223a2019'
+    checksum       = '4E965AEFE0233DD7D936FE92727A3DED6D5779B0ECDAABCA747FBADFCB857E22'
     checksumType   = 'sha256'
     fileType       = 'EXE'
     silentArgs     = '/quiet /update:false'
