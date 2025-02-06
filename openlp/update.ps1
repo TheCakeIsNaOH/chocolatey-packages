aaa38b124a83c6f7fe91fccee24ac332b37270c4
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-02-05 18:12:49 -0600

    Openlp fix bug

--- openlp/update.ps1
+++ openlp/update.ps1
@@ -10,7 +10,6 @@ function global:au_SearchReplace {
 }
 
 function global:au_BeforeUpdate {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
     $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
 }
 
