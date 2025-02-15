bf47e6484fabded83941d553ab928e2edc380be8
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-02-14 21:49:06 -0600

    Try to fix partition assistant standard download
    Fixes #42

--- partition-assistant-standard/tools/chocolateyInstall.ps1
+++ partition-assistant-standard/tools/chocolateyInstall.ps1
@@ -5,11 +5,18 @@ $packageArgs = @{
   packageName	 = $env:ChocolateyPackageName
   fileType		 = 'exe'
   url			 = 'https://www.aomeitech.com/ss/download/pa/PAssist_Std.exe'
-  checksum       = '426261b1fd893c4824a667376495a1373c22fa101683b87281347dffe00ea7d5'
+  checksum       = 'a055bf219d2bf03f2ea253ab776252e49c0e476d787c838c93ac4e56a2fc5930'
   checksumType   = 'sha256'
   silentArgs	 = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   softwareName   = 'AOMEI Partition Assistant Standard*'
   validExitCodes = @(0)
+  options        = @{
+    Headers = @{ "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" }
+  }
 }
+
+Write-Host -ForegroundColor Cyan "Warning messages about failed 'Attempt to get headers' can be ignored."
+Write-Host -ForegroundColor Cyan "These messages are due to a limitation in Chocolatey CLI"
+Write-Host -ForegroundColor Cyan "Go thumbs up the issue: https://github.com/chocolatey/choco/issues/3191"
 
 Install-ChocolateyPackage @packageArgs
