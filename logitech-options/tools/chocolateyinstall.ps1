f6ae16f47227e0f113f145329d9bc68ef9c5a770
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-04-07 21:47:50 -0500

    Update logitech-options, disable broken update scripts

--- logitech-options/tools/chocolateyinstall.ps1
+++ logitech-options/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ Confirm-Win10
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = 'https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe'
-    checksum       = '4E965AEFE0233DD7D936FE92727A3DED6D5779B0ECDAABCA747FBADFCB857E22'
+    checksum       = '588f179a3daa3690f2afad61409393f26cabc00d580f3e78a7c9548c5f350fb2'
     checksumType   = 'sha256'
     fileType       = 'EXE'
     silentArgs     = '/quiet /update:false'
