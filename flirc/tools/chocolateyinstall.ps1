391ce941d988394f79710ccea9ead5bf1c656fcf
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-07-01 22:10:39 -0500

    Add flirc

---
+++ flirc/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Flirc*'
+  FileType      = 'EXE'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  url32         = 'https://'
+  checksum32    = ''
+  checksumType32= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ flirc/update.ps1
@@ -0,0 +1,26 @@
+Import-Module Chocolatey-AU
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyinstall.ps1" = @{
+      "(?i)(^\s*url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+}
+
+function global:au_GetLatest {
+    $updatesApiUrl = "http://flirc.com/software/release/gui/windows/appcast.xml"
+    [xml]$updatesApiPage = Invoke-WebRequest -Uri $updatesApiUrl -UseBasicParsing
+
+    $url32 = $updatesApiPage.rss.channel.item.enclosure.url
+    $version = $updatesApiPage.rss.channel.item.enclosure.version
+
+    return @{ Version = $version; URL32 = $url32 }
+}
+
+Update-Package -ChecksumFor none
