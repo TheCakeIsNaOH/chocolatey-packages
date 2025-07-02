df1abfc6095f7dae06641b5e563cc2f48161fd49
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-07-02 18:21:57 -0500

    fix flirc

--- flirc/tools/chocolateyinstall.ps1
+++ flirc/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   FileType      = 'EXE'
   silentArgs    = "/S"
   validExitCodes= @(0)
-  url32         = 'https://flirc.com/software/flirc-usb/GUI/release/windows/Flirc-Setup-3.27.16.exe'
+  url           = 'https://flirc.com/software/flirc-usb/GUI/release/windows/Flirc-Setup-3.27.16.exe'
   checksum32    = 'eb1fe29025fabf04d2c1868d3059fc24765791645080341578f9dd164ae70e99'
   checksumType32= 'sha256'
 }
--- flirc/update.ps1
+++ flirc/update.ps1
@@ -3,7 +3,7 @@ Import-Module Chocolatey-AU
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyinstall.ps1" = @{
-      "(?i)(^\s*url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
       "(?i)(^\s*checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
