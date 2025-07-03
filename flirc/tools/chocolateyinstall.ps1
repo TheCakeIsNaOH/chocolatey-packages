91c69f348ef7b27293f979228d42d98bfe9cf9e4
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-07-02 19:12:36 -0500

    fix flirc again

--- flirc/tools/chocolateyinstall.ps1
+++ flirc/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs    = "/S"
   validExitCodes= @(0)
   url           = 'https://flirc.com/software/flirc-usb/GUI/release/windows/Flirc-Setup-3.27.16.exe'
-  checksum32    = 'eb1fe29025fabf04d2c1868d3059fc24765791645080341578f9dd164ae70e99'
+  checksum      = 'eb1fe29025fabf04d2c1868d3059fc24765791645080341578f9dd164ae70e99'
   checksumType32= 'sha256'
 }
 
--- flirc/update.ps1
+++ flirc/update.ps1
@@ -4,7 +4,7 @@ function global:au_SearchReplace {
    @{
         ".\tools\chocolateyinstall.ps1" = @{
       "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
