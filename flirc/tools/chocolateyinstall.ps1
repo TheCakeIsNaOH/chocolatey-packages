877f943fa7c4aa94c88abc264e3cd9e2f7ee3544
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-08-16 16:25:07 -0500

    Flirc fix install script typo

--- flirc/tools/chocolateyinstall.ps1
+++ flirc/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   validExitCodes= @(0)
   url           = 'https://flirc.com/software/flirc-usb/GUI/release/windows/Flirc-Setup-3.27.17.exe'
   checksum      = 'b0a80de7638e21532ade10491068c9d13fba0039bd2b139298688e6d6da1a160'
-  checksumType32= 'sha256'
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
