dcb9d05d415cc30ba69832ee5d7e52900d092aaf
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-12-05 22:41:02 -0600

    Add amd-software-adrenalin-edition

---
+++ amd-software-adrenalin-edition/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir	= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName	 = $env:ChocolateyPackageName
+  fileType		 = 'exe'
+  url			 = 'https://drivers.amd.com/drivers/whql-amd-software-adrenalin-edition-25.11.1-win11-nov-combined.exe'
+  checksum       = 'D623CBF705AE0509E0F4E5B623AAA3C0FBD2C5BCF43930B41C7CE591B0C61EC7'
+  checksumType   = 'sha256'
+  silentArgs	 = '-install'
+  softwareName   = 'AMD Software'
+  validExitCodes = @(0)
+  options        = @{
+    Headers = @{ 
+      "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+      Accept  = '*/*'
+      Referer = 'https://www.amd.com/en/support/download/drivers.html' 
+    }
+  }
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ amd-software-adrenalin-edition/tools/chocolateyuninstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  softwareName  = 'AMD Software'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  silentArgs    = '/EXPRESS_UNINSTALL /IGNORE_UPGRADE /ON_REBOOT_MESSAGE:NO'
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+function Split-CommandLine {
+    param([string] $file)
+    return $file
+}
+
+if ($key.Count -eq 1) {
+  $key | % {   
+    # Remove quotes and trailing arguments if any
+    $packageArgs['file'] = Invoke-Expression "Split-CommandLine $($_.UninstallString)"
+	Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
