abcea58638f7d2b1c3e7afa8a9fe3b4eaa789de6
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2025-12-06 10:56:07 -0600

    Amd driver add AU script

---
+++ amd-software-adrenalin-edition/update.ps1
@@ -0,0 +1,48 @@
+﻿Import-Module Chocolatey-AU
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+		}
+	}
+}
+
+function global:au_BeforeUpdate {
+    $Headers = @{ 
+      "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+      Accept  = '*/*'
+      Referer = 'https://www.amd.com/en/support/download/drivers.html' 
+    }
+
+    $Latest.Checksum64 = Get-RemoteChecksum -Url $Latest.URL64 -Headers $headers
+}
+
+function global:au_AfterUpdate {
+  Update-Metadata -key 'releaseNotes' -value $Latest.ReleaseNotes
+}
+
+function global:au_GetLatest {
+	$download_page = Invoke-WebRequest -Uri "https://www.amd.com/en/support/downloads/drivers.html/graphics/radeon-rx/radeon-rx-9000-series/amd-radeon-rx-9070-xt.html" -UseBasicParsing
+    $novUrl        = $download_page.Links | Where-Object href -like "*.exe" | Select-Object -ExpandProperty href -First 1
+    $combinedUrl   = $novUrl -Replace ".exe","-combined.exe"
+    $novName       = $novUrl -split '/' | Select-Object -Last 1
+    $hasWHQL       = $novName.StartsWith("whql")
+    $versionSplit  = Get-Version $novName
+    $version       = ($versionSplit).Version.ToString()
+    $releaseNotesUrl = "https://www.amd.com" + ($download_page.Links | Where-Object href -like "*RN-RAD-WIN*" | Select-Object -ExpandProperty href -First 1)
+
+
+    if (!($hasWHQL)) {
+        $version = $version + "-optional"
+    }
+	
+	return @{ 
+        Version = $version; 
+        URL64 = $combinedUrl; 
+        ReleaseNotes = $releaseNotesUrl
+    }
+}
+
+Update-Package -ChecksumFor none -NoCheckUrl
