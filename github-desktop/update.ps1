75a348485ea15479d62db7d246a9e5b58db2ce24
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2026-07-01 22:13:43 -0500

    Github desktop find another page for update download links

--- github-desktop/update.ps1
+++ github-desktop/update.ps1
@@ -15,11 +15,18 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Get-GitHubLatestReleaseLinks -User "desktop" -Repository "desktop"
+    #$download_page = Get-GitHubLatestReleaseLinks -User "desktop" -Repository "desktop"
     
-    $url64         = $download_page.Links | Where-Object href -match ".*.exe$" | Select-Object -First 1 -ExpandProperty href
-    $version       = (Get-Version $url64).version.tostring()
-    $modurl        = 'https://github.com' + $url64
+    #$url64         = $download_page.Links | Where-Object href -match ".*.exe$" | Select-Object -First 1 -ExpandProperty href
+    #$version       = (Get-Version $url64).version.tostring()
+    #$modurl        = 'https://github.com' + $url64
+
+    $release_tag_url = Get-RedirectedUrl -url "https://github.com/desktop/desktop/releases/latest"
+    $version         = (Get-Version ($release_tag_url -split "/" | Select-Object -Last 1)).Version.ToString()
+    #$download_page   = Invoke-WebRequest -Usebasicparsing -Uri "https://desktop.githubusercontent.com/?page=1"
+    $download_info   = Invoke-RestMethod -Method Get -Usebasicparsing -Uri "https://central.github.com/deployments/list?page=1"
+    $deployment      = $download_info.deployments | Where-Object version -eq $version
+    $url64           = $deployment.artifacts | Where-Object is_windows -eq "True" | Where-Object Name -Match ".*x64.exe$" | Select-Object -ExpandProperty url -First 1
     
     return @{
         Version = $version
